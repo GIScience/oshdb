39dd5bafc48516b178cb4d9b797d353347a6e7a2
Rafael Troilo <rafael.troilo@uni-heidelberg.de>
2017-04-04 17:08:30 +0200

    move test code to according packaged and class

--- core/hosmdb/src/test/java/org/heigit/bigspatialdata/hosmdb/osh/HOSMNodeTest.java
+++ core/hosmdb/src/test/java/org/heigit/bigspatialdata/hosmdb/osh/HOSMNodeTest.java
@@ -1,24 +1,45 @@
 package org.heigit.bigspatialdata.hosmdb.osh;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.heigit.bigspatialdata.hosmdb.osm.OSMNode;
 import org.junit.Test;
 
 public class HOSMNodeTest {
 
-  @Test
-  public void testGetNodes() {
-    fail("Not yet implemented");
-  }
+	public static final int USER_A = 1;
+	public static final int USER_B = 2;
+	public static final int[] TAGS_A = new int[] { 1, 1 };
+	public static final int[] TAGS_B = new int[] { 2, 2 };
+	public static final long[] LONLAT_A = new long[] { 86756350l, 494186210l };
+	public static final long[] LONLAT_B = new long[] { 87153340l, 494102830l };
 
-  @Test
-  public void testIterator() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testCompact() {
-    fail("Not yet implemented");
-  }
+	
+	@Test
+	public void testBuild() throws IOException{
+		List<OSMNode> versions = new ArrayList<>();
+	    
+	    versions.add(new OSMNode(123l,1,1l,0l,USER_A,TAGS_A,LONLAT_A[0], LONLAT_A[1]));
+	    versions.add(new OSMNode(123l,-2,2l,0l,USER_A,TAGS_A,LONLAT_A[0], LONLAT_A[1]));
+	    
+	    
+	    HOSMNode hnode = HOSMNode.build(versions);    
+	    assertNotNull(hnode);
+	    
+	    List<OSMNode> v = hnode.getVersions();
+	    assertNotNull(v);
+	    assertEquals(2, v.size());
+	}
+	
+	@Test
+	public void testCompact() {
+		fail("Not yet implemented");
+	}
 
 }
