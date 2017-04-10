7f64810e7ab9993909a239b9f9be69f7ce1703c5
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-04-10 15:46:47 +0200

    add class for geo helper methods

--- core/hosmdb/src/main/java/org/heigit/bigspatialdata/hosmdb/util/Geo.java
+++ core/hosmdb/src/main/java/org/heigit/bigspatialdata/hosmdb/util/Geo.java
@@ -1,7 +1,20 @@
 package org.heigit.bigspatialdata.hosmdb.util;
 
 /**
- * Created by tyr on 10/04/17.
+ * Geometry utility functions
  */
 public class Geo {
+
+	public static double distanceBetweenInM(double lat1, double lng1, double lat2, double lng2) {
+		// todo: replace with simpler approximation (assuming segments are typically nearby)
+		double earthRadius = 6371000; //meters
+		double dLat = Math.toRadians(lat2-lat1);
+		double dLng = Math.toRadians(lng2-lng1);
+		double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+			Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLng/2) * Math.sin(dLng/2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+		return earthRadius * c;
+	}
+
 }
