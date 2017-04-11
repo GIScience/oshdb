e7a98d76a6e74117fb58fc044c77c8589e5f652a
Moritz <m.schott@stud.uni-heidelberg.de>
2017-04-11 14:56:25 +0200

    making resource available in pbf-parser-test

--- core/oshpbf-parser/src/test/java/BlockCount.java
+++ core/oshpbf-parser/src/test/java/BlockCount.java
@@ -1,42 +1,40 @@
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Spliterator;
-
-import org.heigit.bigspatialdata.oshpbf.OSHPbfParser;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.heigit.bigspatialdata.oshpbf.OshPbfIterator;
-import org.heigit.bigspatialdata.oshpbf.OsmPbfBlocks;
 import org.heigit.bigspatialdata.oshpbf.OsmPbfIterator;
 import org.heigit.bigspatialdata.oshpbf.OsmPrimitiveBlockIterator;
 
 public class BlockCount {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
-		String filename = "target/test-classes/data/venice.osh.pbf";
-		
-	
-		Spliterator<String> ou;
-		
-		
-		
-		final OsmPrimitiveBlockIterator pbfBlock = new OsmPrimitiveBlockIterator(filename);
-		final OsmPbfIterator osmIterator = new OsmPbfIterator(pbfBlock);
-		final OshPbfIterator oshIterator = new OshPbfIterator(osmIterator);
-		
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+    try {
+      String filename = new File(BlockCount.class.getResource("/org/heigit/bigspatialdata/oshpbf/mapreduce/maldives.osh.pbf").toURI()).toString();
 
-		
-		
-		int countBlocks = 0;
-		System.out.println(pbfBlock.getHeaderInfo());
-		
-		while(pbfBlock.hasNext()){
-			pbfBlock.next();
-			countBlocks++;
-		}
-		
-		
-		System.out.println("Count "+countBlocks);
+      Spliterator<String> ou;
 
-	}
+      final OsmPrimitiveBlockIterator pbfBlock = new OsmPrimitiveBlockIterator(filename);
+      final OsmPbfIterator osmIterator = new OsmPbfIterator(pbfBlock);
+      final OshPbfIterator oshIterator = new OshPbfIterator(osmIterator);
+
+      int countBlocks = 0;
+      System.out.println(pbfBlock.getHeaderInfo());
+
+      while (pbfBlock.hasNext()) {
+        pbfBlock.next();
+        countBlocks++;
+      }
+
+      System.out.println("Count " + countBlocks);
+    } catch (URISyntaxException ex) {
+      Logger.getLogger(BlockCount.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+  }
 
 }
--- core/oshpbf-parser/src/test/java/FindNode.java
+++ core/oshpbf-parser/src/test/java/FindNode.java
@@ -1,7 +1,11 @@
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.heigit.bigspatialdata.oshpbf.OshPbfIterator;
 import org.heigit.bigspatialdata.oshpbf.OsmPbfIterator;
 import org.heigit.bigspatialdata.oshpbf.OsmPrimitiveBlockIterator;
@@ -11,26 +15,28 @@ import org.heigit.bigspatialdata.oshpbf.osm.OSMPbfEntity.Type;
 public class FindNode {
 
   public static void main(String[] args) throws FileNotFoundException, IOException {
-    
-    
-    
-    final String filename = "/home/rtroilo/heigit_git/data/venice.osh.pbf";
-    final OsmPrimitiveBlockIterator pbfBlock = new OsmPrimitiveBlockIterator(filename);
-    final OsmPbfIterator osmIterator = new OsmPbfIterator(pbfBlock);
-    final OshPbfIterator oshIterator = new OshPbfIterator(osmIterator);
-    
-    while(oshIterator.hasNext()){
-      List<OSMPbfEntity> e = oshIterator.next();
-      
-      if(e.get(0).getId() == 883702275){
-        System.out.println(e);
-        break;
+
+    try {
+      final String filename = new File(BlockCount.class.getResource("/org/heigit/bigspatialdata/oshpbf/mapreduce/maldives.osh.pbf").toURI()).toString();
+      final OsmPrimitiveBlockIterator pbfBlock = new OsmPrimitiveBlockIterator(filename);
+      final OsmPbfIterator osmIterator = new OsmPbfIterator(pbfBlock);
+      final OshPbfIterator oshIterator = new OshPbfIterator(osmIterator);
+
+      while (oshIterator.hasNext()) {
+        List<OSMPbfEntity> e = oshIterator.next();
+
+        if (e.get(0).getId() == 883702275) {
+          System.out.println(e);
+          break;
+        }
+
+        if (e.get(0).getType() == Type.WAY) {
+          break;
+        }
       }
-       
-      if(e.get(0).getType() == Type.WAY)
-        break;
+    } catch (URISyntaxException ex) {
+      Logger.getLogger(FindNode.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
 
   }
 
