a405cdea6a9f0205605de4d69894cc8eccd9965a
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-04-21 11:32:44 +0200

    remove unneccessary imports

--- core/hosmdb/src/main/java/org/heigit/bigspatialdata/hosmdb/db/HOSMDb.java
+++ core/hosmdb/src/main/java/org/heigit/bigspatialdata/hosmdb/db/HOSMDb.java
@@ -1,16 +1,5 @@
 package org.heigit.bigspatialdata.hosmdb.db;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
-import org.heigit.bigspatialdata.hosmdb.osh.HOSMNode;
-import org.heigit.bigspatialdata.hosmdb.osh.HOSMWay;
-import org.heigit.bigspatialdata.hosmdb.osm.OSMNode;
-import org.heigit.bigspatialdata.hosmdb.osm.OSMWay;
-
 public class HOSMDb {
 
 	public static final int MAXZOOM = 12;
