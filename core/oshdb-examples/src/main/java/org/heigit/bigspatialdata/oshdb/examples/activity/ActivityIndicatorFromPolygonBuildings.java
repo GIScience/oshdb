77699661d08e3752674b436d9576ceec95bc4cf3
Michael Auer <michael.auer@geog.uni-heidelberg.de>
2017-05-21 23:12:09 +0200

    added ActivityIndicator for buildings

---
+++ core/oshdb-examples/src/main/java/org/heigit/bigspatialdata/oshdb/examples/activity/ActivityIndicatorFromPolygonBuildings.java
@@ -0,0 +1,231 @@
+package org.heigit.bigspatialdata.oshdb.examples.activity;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TimeZone;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import org.geotools.geometry.jts.JTS;
+import org.heigit.bigspatialdata.oshdb.grid.GridOSHEntity;
+import org.heigit.bigspatialdata.oshdb.index.XYGridTree;
+import org.heigit.bigspatialdata.oshdb.osh.*;
+import org.heigit.bigspatialdata.oshdb.osm.*;
+import org.heigit.bigspatialdata.oshdb.util.BoundingBox;
+import org.heigit.bigspatialdata.oshdb.util.CellId;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.TagInterpreter;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.io.ParseException;
+
+public class ActivityIndicatorFromPolygonBuildings {
+  
+  /**
+   * Computes an Activity Indicator for buildings inside a polygon.
+   * This activity indicator counts all changes of building objects,which are geometry changes + tag changes (way versions) + tag and relation_member changes (relation versions)
+   * @param conn connection object to OSHDB e.g h2
+   * @param polygon to define analyis area
+   * @param ti TagInterpreter to decide whether to build polygons or linestrings
+   * @return Map<Long,Long> with timestamps and counts
+   * @throws ClassNotFoundException
+   * @throws ParseException
+   * @throws IOException
+   */
+	public Map<Long,Long> execute(Connection conn, Geometry polygon, TagInterpreter ti) throws ClassNotFoundException, ParseException, IOException {
+		Class.forName("org.h2.Driver");
+		
+		//FileWriter fw = new FileWriter("allbuildingversions.csv");
+		
+
+		// create list of timestamps to analyze
+		List<Long> timestamps = new ArrayList<>();
+		final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+		for (int year = 2016; year <= 2016; year++) {
+			for (int month = 1; month <= 12; month++) {
+				try {
+					timestamps.add(formatter.parse(String.format("%d%02d01", year, month)).getTime()/1000);
+				} catch (java.text.ParseException e) {
+					System.err.println("basdoawrd");
+				}
+				;
+			}
+		}
+		Collections.sort(timestamps, Collections.reverseOrder());
+		
+		
+
+		//create BBOX to query gridcells as CellId-Objects, which contain (zoomlevel,id)
+    MultiPolygon inputPolygon = (MultiPolygon) polygon;
+    
+    Double minLon = JTS.toEnvelope(inputPolygon).getMinX();
+    Double maxLon = JTS.toEnvelope(inputPolygon).getMaxX();
+    Double minLat = JTS.toEnvelope(inputPolygon).getMinY();
+    Double maxLat = JTS.toEnvelope(inputPolygon).getMaxY();
+    
+    BoundingBox inputBbox = new BoundingBox(minLon, maxLon, minLat, maxLat);
+    
+		XYGridTree grid = new XYGridTree(12);
+
+		final List<CellId> cellIds = new ArrayList<>();
+		
+		grid.bbox2CellIds(inputBbox, true).forEach(cell -> {
+				cellIds.add(cell);
+		});
+
+		//start processing in parallel all grid cells that relate to the input polygons BBOX 
+			Map<Long,Long> superresult = cellIds.parallelStream().flatMap(cellId -> {
+			  
+				try (final PreparedStatement pstmt = conn
+						.prepareStatement("(select data from grid_way where level = ?1 and id = ?2) union (select data from grid_relation where level = ?1 and id = ?2)")) {
+					pstmt.setInt(1, cellId.getZoomLevel());
+					pstmt.setLong(2, cellId.getId());
+
+					//each time 2 gridCells (1 grid_way and 1 grid_relation) are queried and put into a stream
+					try (final ResultSet rst2 = pstmt.executeQuery()) {
+						List<GridOSHEntity> cells = new LinkedList<>();
+						while (rst2.next()) {
+							final ObjectInputStream ois = new ObjectInputStream(rst2.getBinaryStream(1));
+							cells.add((GridOSHEntity) ois.readObject());
+							//final GridOSHEntity gridCell = (GridOSHEntity) ois.readObject();
+							//return gridCell;
+						}
+//						else {
+//						  return null;
+//						}
+						return cells.stream();
+						
+					}
+				} catch (IOException | SQLException | ClassNotFoundException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}).map(gridCell -> {
+
+			  
+				GridOSHEntity cell =  (GridOSHEntity) gridCell;
+
+				Map<Long, Long> timestampActivity = new TreeMap<>();
+		
+				Iterator<OSHEntity> itr = cell.iterator();
+
+				while (itr.hasNext()) {
+					OSHEntity<OSMEntity> osh = itr.next();
+ 
+					if (!  osh.getBoundingBox().getGeometry().intersects(inputPolygon)) {
+						continue; 
+						}
+ 
+					List<OSMEntity> versions = new ArrayList<>();
+					
+					List<Long> modTs = osh.getModificationTimestamps(true);
+					Collections.sort(modTs, Collections.reverseOrder());
+					
+					for (Long t : modTs) {
+
+          OSMEntity osm = osh.getByTimestamp(t);
+          try {
+            if (osm.isVisible() && osm.hasTagKey(0) && osm.getGeometry(t, ti).isValid()
+                && osm.getGeometry(t, ti).isSimple() && osm.getGeometry(t, ti).getCentroid().intersects(inputPolygon)) {
+
+//              try {
+//                fw.append(osm.getGeometry(t, ti).toText().toString() + "\n");
+//              } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//              }
+
+              versions.add(osm);
+            }
+          } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e);
+          }
+        }
+					
+					int v = 0;
+					for (int i = 0; i < timestamps.size(); i++) {
+						long ts = timestamps.get(i);
+						long count = 0;
+						while (v < versions.size() && versions.get(v).getTimestamp() > ts) {
+							count++;
+							v++;
+						}
+						if (i==0){
+							continue;
+						}
+						if (timestampActivity.containsKey(ts)) {
+
+							timestampActivity.put(ts, timestampActivity.get(ts) + count);
+
+						} else {
+							timestampActivity.put(ts, count);
+						}
+
+						if (v >= versions.size())
+							break;
+
+						
+					}
+					
+				}
+				
+				return timestampActivity;
+				
+			}).reduce(Collections.emptyMap(),(partial, b) -> 
+			{
+			 
+			  Map<Long, Long> sum = new TreeMap<>();
+				sum.putAll(partial);
+				for(Map.Entry<Long, Long> entry : b.entrySet()){
+				
+					Long activity = partial.get(entry.getKey());
+					if(activity==null){
+					
+					  activity = entry.getValue();
+					  
+            if (activity == null) {
+              activity = (long) 0; 
+            }
+					  
+					}
+					else{
+					  Long newActivity = entry.getValue().longValue();
+//					  if (newActivity == null){ newActivity =  Long.valueOf(0); }
+						activity= Long.valueOf((activity.longValue() + newActivity));
+					
+					}
+					sum.put(entry.getKey(), activity);
+				}
+				
+//				System.out.println(sum);
+						
+				return sum;
+			}
+			
+			);
+			
+			//fill missing values with 0
+			for (Long ts : timestamps.subList(1, timestamps.size())) {
+			  superresult.putIfAbsent(ts, (long) 0);
+      }
+			
+			return superresult;	
+
+	}
+
+}
--- core/oshdb-examples/src/main/java/org/heigit/bigspatialdata/oshdb/examples/osmatrix/ISEA3HGrid.java
+++ core/oshdb-examples/src/main/java/org/heigit/bigspatialdata/oshdb/examples/osmatrix/ISEA3HGrid.java
@@ -5,78 +5,142 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureSource;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
+import org.geotools.geometry.text.WKTParser;
 import org.heigit.bigspatialdata.oshdb.examples.activity.ActivityIndicatorFromPolygon;
+import org.heigit.bigspatialdata.oshdb.examples.activity.ActivityIndicatorFromPolygonBuildings;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.DefaultTagInterpreter;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.TagInterpreter;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.Filter;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
+import com.vividsolutions.jts.io.WKTWriter;
 
 public class ISEA3HGrid {
+  public static void main(String[] args) throws IOException, SQLException, IndexOutOfBoundsException, ParseException,
+      ClassNotFoundException, org.json.simple.parser.ParseException {
+    System.out.println("Start: " + new Date().toString());
+    // create filewriter to output results
+    FileWriter fw = new FileWriter("hd_activity.csv");
+    
+    // load shapefile and get features
+    File file = new File("data/grid19_hd_ISEA3H_poly.shp");
+    // File file = new File("data/heidelberg_boundary.shp");
+    Map<String, Object> map = new HashMap<>();
+    map.put("url", file.toURI().toURL());
 
-	public static void main(String[] args) throws IOException, SQLException, IndexOutOfBoundsException, ParseException, ClassNotFoundException {
-		// TODO Auto-generated method stub
+    DataStore dataStore = DataStoreFinder.getDataStore(map);
+    String typeName = dataStore.getTypeNames()[0];
 
-		//load shapefile
-		File file = new File("data/grid19_hd_ISEA3H_poly.shp");
-	    Map<String, Object> map = new HashMap<>();
-	    map.put("url", file.toURI().toURL());
+    FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
+    Filter filter = Filter.INCLUDE;
 
-	    DataStore dataStore = DataStoreFinder.getDataStore(map);
-	    String typeName = dataStore.getTypeNames()[0];
+    // connect to Database
 
-	    FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore
-	            .getFeatureSource(typeName);
-	    Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
+    // tcp://localhost
+    Connection conn = DriverManager
+        .getConnection("jdbc:h2:/d:/eclipseNeon2Workspace/OSH-BigDB/core/hosmdb/resources/oshdb/heidelberg", "sa", "");
+    
+    //create lookupmap of tags and TagInterpreter for getGeometry() methods
+    final Statement stmt = conn.createStatement();
+    ResultSet rstTags = stmt.executeQuery(
+        "select k.ID as KEYID, kv.VALUEID as VALUEID, k.txt as KEY, kv.txt as VALUE from KEYVALUE kv inner join KEY k on k.ID = kv.KEYID;");
+    Map<String, Map<String, Pair<Integer, Integer>>> allKeyValues = new HashMap<>();
+    while (rstTags.next()) {
+      int keyId = rstTags.getInt(1);
+      int valueId = rstTags.getInt(2);
+      String keyStr = rstTags.getString(3);
+      String valueStr = rstTags.getString(4);
+      if (!allKeyValues.containsKey(keyStr))
+        allKeyValues.put(keyStr, new HashMap<>());
+      allKeyValues.get(keyStr).put(valueStr, new ImmutablePair<>(keyId, valueId));
+    }
+    rstTags.close();
+    ResultSet rstRoles = stmt.executeQuery("select ID as ROLEID, txt as ROLE from ROLE;");
+    Map<String, Integer> allRoles = new HashMap<>();
+    while (rstRoles.next()) {
+      int roleId = rstRoles.getInt(1);
+      String roleStr = rstRoles.getString(2);
+      allRoles.put(roleStr, roleId);
+    }
+    rstRoles.close();
+    final TagInterpreter tagInterpreter = new DefaultTagInterpreter(allKeyValues, allRoles);
 
-	    Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/d:/eclipseNeon2Workspace/OSH-BigDB/core/hosmdb/resources/oshdb/heidelberg-ccbysa","sa", "");
-	    
-	    WKTReader reader = new WKTReader();
+    
+    // fill features into parallelizable List
+    List<Geometry> list = new ArrayList<>();
+    FeatureCollection<SimpleFeatureType, SimpleFeature> fcollection = source.getFeatures(filter);
 
-		FileWriter fw = new FileWriter("hd_activity.csv");
-		
-		
-	    
-	    FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
-	    
-	    List<Geometry> list = new ArrayList<>();
-		
-	    try (FeatureIterator<SimpleFeature> features = collection.features()) {
-	        while (features.hasNext()) {
-	            SimpleFeature feature = features.next();
-//	            System.out.print(feature.getID());
-//	            System.out.print(": ");
-//	            System.out.println(feature.getDefaultGeometryProperty().getValue());
-	           // System.out.println( feature.getAttribute(0).toString() );
-	            Geometry multipolygon = reader.read(feature.getAttribute(0).toString());
-	            list.add(multipolygon);
-	            
-	            
-//	            System.out.println(feature.getAttribute(1) +";" +  cellresult.get(Long.valueOf(1451606400)) + ";" + feature.getAttribute(0).toString());
-	        }
-	        
-	        list.parallelStream().map(multipoly -> {
-	        	ActivityIndicatorFromPolygon aifp = new ActivityIndicatorFromPolygon();
-	            Map<Long,Long> cellresult = aifp.execute(conn, multipolygon);
-	            fw.append(feature.getAttribute(1) +";" +  cellresult.get(Long.valueOf(1451606400)) + ";" + feature.getAttribute(0).toString() + "\n");
-	        return null;
-	        }).sum();
-	    }
-		fw.close();
-		System.out.println("Done");
-	}
+    try (FeatureIterator<SimpleFeature> features = fcollection.features()) {
+      while (features.hasNext()) {
+
+        SimpleFeature feature = features.next();
+        Geometry multipolygon = (Geometry) feature.getDefaultGeometry();
+        list.add(multipolygon);
+
+      }
+    }
+
+    
+    //execute ActivityIndicator in parallel for all features of shapefile
+    Optional<String> superresult = list.parallelStream().map(multipoly -> {
+      ActivityIndicatorFromPolygonBuildings aifp = new ActivityIndicatorFromPolygonBuildings();
+      Map<Long, Long> cellresult;
+      StringBuilder sb = new StringBuilder();
+      try {
+        //get activities for feature
+        cellresult = aifp.execute(conn, multipoly, tagInterpreter);
+        
+        //create output: 1st column: WKT geometry
+        sb.append(multipoly.toString());
+        
+        //create output: rest of columns activity values
+        for (Map.Entry<Long, Long> timePeriod : cellresult.entrySet()) {
+          sb.append(";" + timePeriod.getValue());
+        }
+        //create output: prepare next csv line by adding a linebreak after the values
+        sb.append("\n");
+      } catch (ClassNotFoundException | ParseException | IOException e) {
+
+        e.printStackTrace();
+      }
+
+      return sb.toString();
+    }).reduce((a, b) -> {
+
+      a = a + b;
+
+      return a.toString();
+    });
+    
+    //write out the whole csv file
+    fw.append(superresult.get());
+    
+    fw.close();
+    System.out.println("Done: " + new Date().toString());
+  }
 
 }
