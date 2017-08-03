760822a036c4cec7aadcf66a69ee4ed755e883c7
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-08-03 10:08:45 +0200

    re-introduce MapperFactory as abstract base class

--- core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapper/OSMContributionMapper.java
+++ core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapper/OSMContributionMapper.java
@@ -3,7 +3,7 @@ package org.heigit.bigspatialdata.oshdb.api.mapper;
 import org.heigit.bigspatialdata.oshdb.OSHDB;
 import org.heigit.bigspatialdata.oshdb.api.objects.OSMContribution;
 
-public class OSMContributionMapper {
+public class OSMContributionMapper extends MapperFactory {
   public static Mapper<OSMContribution> using(OSHDB oshdb) {
     return Mapper.<OSMContribution>using(oshdb, OSMContribution.class);
   }
