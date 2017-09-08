7223abba5746ba1bde4c72520a6577cb1ae5e432
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-09-08 10:55:38 +0200

    implment remaining *GroupedById flatMap of multithread H2 backend

--- core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapper/Mapper_H2_multithread.java
+++ core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapper/Mapper_H2_multithread.java
@@ -41,7 +41,7 @@ public class Mapper_H2_multithread<T> extends Mapper<T> {
     if (this._tagTranslator == null) this._tagTranslator = new TagTranslator(((OSHDB_H2) this._oshdbForTags).getConnection());
     return this._tagTranslator.tag2Int(new ImmutablePair(key,value));
   }
-  
+
   @Override
   protected <R, S> S mapReduceCellsOSMContribution(Iterable<CellId> cellIds, List<Long> tstamps, BoundingBox bbox, Polygon poly, SerializablePredicate<OSHEntity> preFilter, SerializablePredicate<OSMEntity> filter, SerializableFunction<OSMContribution, R> mapper, SerializableSupplier<S> identitySupplier, SerializableBiFunction<S, R, S> accumulator, SerializableBinaryOperator<S> combiner) throws Exception {
     //load tag interpreter helper which is later used for geometry building
@@ -51,71 +51,70 @@ public class Mapper_H2_multithread<T> extends Mapper<T> {
     cellIds.forEach(cellIdsList::add);
 
     return cellIdsList.parallelStream()
-    .flatMap(cell -> {
-      try {
-        // fetch data from H2 DB
-        PreparedStatement pstmt = ((OSHDB_H2) this._oshdb).getConnection().prepareStatement(
-            (this._typeFilter.contains(OSMType.NODE) ? "(select data from grid_node where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
-                " union all " +
-                (this._typeFilter.contains(OSMType.WAY) ? "(select data from grid_way where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
-                " union all " +
-                (this._typeFilter.contains(OSMType.RELATION) ? "(select data from grid_relation where level = ?1 and id = ?2)" : "(select 0 as data where false)" )
-        );
-        pstmt.setInt(1, cell.getZoomLevel());
-        pstmt.setLong(2, cell.getId());
-        ResultSet oshCellsRawData = pstmt.executeQuery();
+        .flatMap(cell -> {
+          try {
+            // fetch data from H2 DB
+            PreparedStatement pstmt = ((OSHDB_H2) this._oshdb).getConnection().prepareStatement(
+                (this._typeFilter.contains(OSMType.NODE) ? "(select data from grid_node where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
+                    " union all " +
+                    (this._typeFilter.contains(OSMType.WAY) ? "(select data from grid_way where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
+                    " union all " +
+                    (this._typeFilter.contains(OSMType.RELATION) ? "(select data from grid_relation where level = ?1 and id = ?2)" : "(select 0 as data where false)" )
+            );
+            pstmt.setInt(1, cell.getZoomLevel());
+            pstmt.setLong(2, cell.getId());
+            ResultSet oshCellsRawData = pstmt.executeQuery();
 
-        // iterate over the result
-        List<GridOSHEntity> cellsData = new ArrayList<>();
-        while (oshCellsRawData.next()) {
-          // get one cell from the raw data stream
-          GridOSHEntity oshCellRawData = (GridOSHEntity) (new ObjectInputStream(oshCellsRawData.getBinaryStream(1))).readObject();
-          cellsData.add(oshCellRawData);
-        }
-        return cellsData.stream();
-      } catch (SQLException | IOException | ClassNotFoundException e) {
-        e.printStackTrace();
-        return Stream.empty();
-      }
-    }).map(oshCell -> {
+            // iterate over the result
+            List<GridOSHEntity> cellsData = new ArrayList<>();
+            while (oshCellsRawData.next()) {
+              // get one cell from the raw data stream
+              GridOSHEntity oshCellRawData = (GridOSHEntity) (new ObjectInputStream(oshCellsRawData.getBinaryStream(1))).readObject();
+              cellsData.add(oshCellRawData);
+            }
+            return cellsData.stream();
+          } catch (SQLException | IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return Stream.empty();
+          }
+        }).map(oshCell -> {
 
-      // iterate over the history of all OSM objects in the current cell
-      List<R> rs = new ArrayList<>();
-      CellIterator.iterateAll(
-          oshCell,
-          bbox,
-          poly,
-          new CellIterator.TimestampInterval(tstamps.get(0), tstamps.get(tstamps.size()-1)),
-          this._tagInterpreter,
-          preFilter,
-          filter,
-          false
-      ).forEach(contribution -> rs.add(
-          mapper.apply(
-              new OSMContribution(
-                  new OSHDBTimestamp(contribution.timestamp),
-                  contribution.nextTimestamp != null ? new OSHDBTimestamp(contribution.nextTimestamp) : null,
-                  contribution.previousGeometry,
-                  contribution.geometry,
-                  contribution.previousOsmEntity,
-                  contribution.osmEntity,
-                  contribution.activities
+          // iterate over the history of all OSM objects in the current cell
+          List<R> rs = new ArrayList<>();
+          CellIterator.iterateAll(
+              oshCell,
+              bbox,
+              poly,
+              new CellIterator.TimestampInterval(tstamps.get(0), tstamps.get(tstamps.size()-1)),
+              this._tagInterpreter,
+              preFilter,
+              filter,
+              false
+          ).forEach(contribution -> rs.add(
+              mapper.apply(
+                  new OSMContribution(
+                      new OSHDBTimestamp(contribution.timestamp),
+                      contribution.nextTimestamp != null ? new OSHDBTimestamp(contribution.nextTimestamp) : null,
+                      contribution.previousGeometry,
+                      contribution.geometry,
+                      contribution.previousOsmEntity,
+                      contribution.osmEntity,
+                      contribution.activities
+                  )
               )
-          )
-      ));
+          ));
 
-      // todo: replace this with `rs.stream().reduce(identitySupplier, accumulator, combiner);` (needs accumulator to be non-interfering and stateless, see http://download.java.net/java/jdk9/docs/api/java/util/stream/Stream.html#reduce-U-java.util.function.BiFunction-java.util.function.BinaryOperator-)
-      S accInternal = identitySupplier.get();
-      // fold the results
-      for (R r : rs) {
-        accInternal = accumulator.apply(accInternal, r);
-      }
-      return accInternal;
-    }).reduce(identitySupplier.get(), (cur, acc) -> {
-      return combiner.apply(acc, cur);
-    });
+          // todo: replace this with `rs.stream().reduce(identitySupplier, accumulator, combiner);` (needs accumulator to be non-interfering and stateless, see http://download.java.net/java/jdk9/docs/api/java/util/stream/Stream.html#reduce-U-java.util.function.BiFunction-java.util.function.BinaryOperator-)
+          S accInternal = identitySupplier.get();
+          // fold the results
+          for (R r : rs) {
+            accInternal = accumulator.apply(accInternal, r);
+          }
+          return accInternal;
+        }).reduce(identitySupplier.get(), (cur, acc) -> {
+          return combiner.apply(acc, cur);
+        });
   }
-
 
   @Override
   protected <R, S> S flatMapReduceCellsOSMContributionGroupedById(Iterable<CellId> cellIds, List<Long> tstamps, BoundingBox bbox, Polygon poly, SerializablePredicate<OSHEntity> preFilter, SerializablePredicate<OSMEntity> filter, SerializableFunction<List<OSMContribution>, List<R>> mapper, SerializableSupplier<S> identitySupplier, SerializableBiFunction<S, R, S> accumulator, SerializableBinaryOperator<S> combiner) throws Exception {
@@ -263,5 +262,76 @@ public class Mapper_H2_multithread<T> extends Mapper<T> {
     }).reduce(identitySupplier.get(), (cur, acc) -> {
       return combiner.apply(acc, cur);
     });
+  }
+
+  @Override
+  protected <R, S> S flatMapReduceCellsOSMEntitySnapshotGroupedById(Iterable<CellId> cellIds, List<Long> tstamps, BoundingBox bbox, Polygon poly, SerializablePredicate<OSHEntity> preFilter, SerializablePredicate<OSMEntity> filter, SerializableFunction<List<OSMEntitySnapshot>, List<R>> mapper, SerializableSupplier<S> identitySupplier, SerializableBiFunction<S, R, S> accumulator, SerializableBinaryOperator<S> combiner) throws Exception {
+    //load tag interpreter helper which is later used for geometry building
+    if (this._tagInterpreter == null)
+      this._tagInterpreter = DefaultTagInterpreter.fromH2(((OSHDB_H2) this._oshdbForTags).getConnection());
+
+    final List<CellId> cellIdsList = new ArrayList<>();
+    cellIds.forEach(cellIdsList::add);
+
+    return cellIdsList.parallelStream()
+        .flatMap(cell -> {
+          try {
+            // fetch data from H2 DB
+            PreparedStatement pstmt = ((OSHDB_H2) this._oshdb).getConnection().prepareStatement(
+                (this._typeFilter.contains(OSMType.NODE) ? "(select data from grid_node where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
+                    " union all " +
+                    (this._typeFilter.contains(OSMType.WAY) ? "(select data from grid_way where level = ?1 and id = ?2)" : "(select 0 as data where false)" ) +
+                    " union all " +
+                    (this._typeFilter.contains(OSMType.RELATION) ? "(select data from grid_relation where level = ?1 and id = ?2)" : "(select 0 as data where false)" )
+            );
+            pstmt.setInt(1, cell.getZoomLevel());
+            pstmt.setLong(2, cell.getId());
+            ResultSet oshCellsRawData = pstmt.executeQuery();
+
+            // iterate over the result
+            List<GridOSHEntity> cellsData = new ArrayList<>();
+            while (oshCellsRawData.next()) {
+              // get one cell from the raw data stream
+              GridOSHEntity oshCellRawData = (GridOSHEntity) (new ObjectInputStream(oshCellsRawData.getBinaryStream(1))).readObject();
+              cellsData.add(oshCellRawData);
+            }
+            return cellsData.stream();
+          } catch (SQLException | IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return Stream.empty();
+          }
+        }).map(oshCell -> {
+          // iterate over the history of all OSM objects in the current cell
+          List<R> rs = new ArrayList<>();
+          CellIterator.iterateByTimestamps(
+              oshCell,
+              bbox,
+              poly,
+              tstamps,
+              this._tagInterpreter,
+              preFilter,
+              filter,
+              false
+          ).forEach(snapshots -> {
+            List<OSMEntitySnapshot> osmEntitySnapshots = new ArrayList<>(snapshots.size());
+            snapshots.entrySet().forEach(entry -> {
+              OSHDBTimestamp tstamp = new OSHDBTimestamp(entry.getKey());
+              Geometry geometry = entry.getValue().getRight();
+              OSMEntity entity = entry.getValue().getLeft();
+              osmEntitySnapshots.add(new OSMEntitySnapshot(tstamp, geometry, entity));
+            });
+            rs.addAll(mapper.apply(osmEntitySnapshots));
+          });
+
+          // todo: replace this with `rs.stream().reduce(identitySupplier, accumulator, combiner);` (needs accumulator to be non-interfering and stateless, see http://download.java.net/java/jdk9/docs/api/java/util/stream/Stream.html#reduce-U-java.util.function.BiFunction-java.util.function.BinaryOperator-)
+          S accInternal = identitySupplier.get();
+          // fold the results
+          for (R r : rs) {
+            accInternal = accumulator.apply(accInternal, r);
+          }
+          return accInternal;
+        }).reduce(identitySupplier.get(), (cur, acc) -> {
+          return combiner.apply(acc, cur);
+        });
   }
 }
