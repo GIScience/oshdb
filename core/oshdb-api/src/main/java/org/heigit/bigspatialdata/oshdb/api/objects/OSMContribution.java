53487def5db5ed845e2b68670253b0ffb61aa4f6
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-09-18 18:45:02 +0200

    write javadoc to getter functions of OSMContribution class

--- core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/objects/OSMContribution.java
+++ core/oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/objects/OSMContribution.java
@@ -10,6 +10,17 @@ import org.heigit.bigspatialdata.oshdb.osm.OSMRelation;
 import org.heigit.bigspatialdata.oshdb.osm.OSMWay;
 import org.heigit.bigspatialdata.oshdb.util.ContributionType;
 
+/**
+ * Holds information about a single modification ("contribution") of a single entity in database.
+ *
+ * It holds the information about:
+ * <ul>
+ *   <li>the timestamp at which this change happened</li>
+ *   <li>state of the entity before and after the modification</li>
+ *   <li>the geometry of the entity before and after the modification</li>
+ *   <li>the type(s) of change which has happened here (e.g. creation/deletion of an entity, modification of a geometry, altering of the tag list, etc.)</li>
+ * </ul>
+ */
 public class OSMContribution {
   private final OSHDBTimestamp _tstamp;
   private final OSHDBTimestamp _validTo;
@@ -28,31 +39,80 @@ public class OSMContribution {
     this._entityAfter = entityAfter;
     this._contributionTypes = contributionTypes;
   }
-  
+
+  /**
+   * Returns the timestamp at which this data modification has happened.
+   *
+   * @return the modification timestamp as a OSHDBTimestamp object
+   */
   public OSHDBTimestamp getTimestamp() {
     return this._tstamp;
   }
-  
+
+  @Deprecated
   public OSHDBTimestamp getValidTo() {
     return this._validTo;
   }
-  
+
+  /**
+   * Returns the geometry of the entity before this modification.
+   * Is `null` if this is a entity creation.
+   *
+   * @return a JTS Geometry object representing the entity's state before the modification
+   */
   public Geometry getGeometryBefore() {
     return this._geometryBefore;
   }
-  
+
+  /**
+   * Returns the geometry of the entity after this modification.
+   * Is `null` if this is a entity deletion.
+   *
+   * @return a JTS Geometry object representing the entity's state after the modification
+   */
   public Geometry getGeometryAfter() {
     return this._geometryAfter;
   }
-  
+
+  /**
+   * Returns the entity object in its state before this modification.
+   * Is `null` if this is a entity creation.
+   *
+   * @return the entity object as it was before this modification
+   */
   public OSMEntity getEntityBefore() {
     return this._entityBefore;
   }
-  
+
+  /**
+   * Returns the entity object in its state after this modification.
+   * If this is a entity deletion, the returned entity will have the visible flag set to false: `entity.getEntityAfter().isVisible == false`
+   *
+   * @return the entity object as it was after this modification
+   */
   public OSMEntity getEntityAfter() {
     return this._entityAfter;
   }
-  
+
+  /**
+   * Determined the type of modification this contribution has made. Can be one or more of:
+   * <ul>
+   *   <li>CREATION</li>
+   *   <li>DELETION</li>
+   *   <li>TAG_CHANGE</li>
+   *   <li>MEMBERLIST_CHANGE</li>
+   *   <li>GEOMETRY_CHANGE</li>
+   * </ul>
+   *
+   * If this is a entity creation or deletion, the other flags are not set (even though one might argue that a just
+   * created object clearly has a different geometry than before, for example).
+   *
+   * The MEMBERLIST_CHANGE flag is set if the list of "direct" children of this object (i.e. the nodes of a way or the
+   * members of a relation, but not for example the nodes of a way of a relation) has changed or rearranged. The flag is
+   * also set if only the role of a member of a relation has been changed.
+   *
+   * @return a set of modification type this contribution made on the underlying data
+   */
   public EnumSet<ContributionType> getContributionTypes() {
     return this._contributionTypes;
   }
