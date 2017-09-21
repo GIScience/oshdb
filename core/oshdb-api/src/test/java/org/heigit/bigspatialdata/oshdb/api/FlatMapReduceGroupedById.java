2088fcfc5c079e0cd1775372b93dbcba72a007ba
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-09-21 11:07:31 +0200

    add first tests for `.flatMapReduceGroupedById()` and `.mapReduce()`
    These  still need to be tested individually for all data views (currently: OSMSnapshotView and OSMContributionView) and endpoints, currently: H2/JDBC singlethreaded, H2/JDBC multithreaded, Ignite (ScanQuery based)!

---
+++ core/oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/FlatMapReduceGroupedById.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.heigit.bigspatialdata.oshdb.api;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.heigit.bigspatialdata.oshdb.OSHDB;
+import org.heigit.bigspatialdata.oshdb.api.db.OSHDB_H2;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapReducer;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.OSMContributionView;
+import org.heigit.bigspatialdata.oshdb.api.objects.OSHDBTimestamps;
+import org.heigit.bigspatialdata.oshdb.api.objects.OSMContribution;
+import org.heigit.bigspatialdata.oshdb.osm.OSMType;
+import org.heigit.bigspatialdata.oshdb.util.BoundingBox;
+import org.heigit.bigspatialdata.oshdb.util.ContributionType;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+public class FlatMapReduceGroupedById {
+  private final OSHDB oshdb;
+
+  private final BoundingBox bbox = new BoundingBox(8, 9, 49, 50);
+  private final OSHDBTimestamps timestamps72 = new OSHDBTimestamps(2010, 2015, 1, 12);
+
+  private final double DELTA = 1e-8;
+
+  public FlatMapReduceGroupedById() throws Exception {
+    oshdb = new OSHDB_H2("./src/test/resources/test-data;ACCESS_MODE_DATA=r");
+  }
+
+  private MapReducer<OSMContribution> createMapReducerOSMContribution() throws Exception {
+    return OSMContributionView.on(oshdb).osmTypes(OSMType.NODE).filterByTagKey("highway").areaOfInterest(bbox);
+  }
+
+  @Test
+  public void test() throws Exception {
+    Integer result = createMapReducerOSMContribution()
+        .timestamps(timestamps72)
+        .flatMapReduceGroupedById(
+            contributions -> {
+              if (contributions.get(0).getEntityAfter().getId() != 617308093)
+                return new ArrayList<>();
+              List<Integer> ret = new ArrayList<>();
+              ret.add((int)contributions.stream().filter(c -> c.getContributionTypes().contains(ContributionType.GEOMETRY_CHANGE)).count());
+              ret.add(2);
+              return ret;
+            },
+            () -> 0,
+            (x,y) -> x + y,
+            (x,y) -> x + y
+        );
+
+    assertEquals(5+2, result.intValue());
+  }
+}
