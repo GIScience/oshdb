7a1d714b192ccfa303a095e8989001d9965a2a87
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-12-05 18:18:32 +0100

    add implementation details - note for developers - to javadoc

--- oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapreducer/MapperFactory.java
+++ oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/mapreducer/MapperFactory.java
@@ -8,11 +8,14 @@ package org.heigit.bigspatialdata.oshdb.api.mapreducer;
  * called "on" with the following structure:
  *
  * {@code
- *   public static MapReducer<R> on(OSHDB oshdb) {…}
+ *   public static MapReducer<R> on(OSHDB_Implementation oshdb[, …]) {…}
  * }
  *
  * where R is the class of the objects that are passed though to the reduce
  * function in the particular mapper implementation (e.g. OSMEntitySnapshot)
+ *
+ * (note for developers: this would have been an abstract static method of this
+ * abstract class, but Java doesn't seem to allow that)
  */
 public abstract class MapperFactory {
 }
