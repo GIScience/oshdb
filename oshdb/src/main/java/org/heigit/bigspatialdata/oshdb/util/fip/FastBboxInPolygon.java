d11d36bd2521bfc17c8a7e9d41c8b9451f3218fb
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-12-18 16:00:56 +0100

    add tests, fix special case -> bbox fully covering inner hole

--- oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/fip/FastBboxInPolygon.java
+++ oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/fip/FastBboxInPolygon.java
@@ -1,11 +1,12 @@
 package org.heigit.bigspatialdata.oshdb.util.fip;
 
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.Polygonal;
+import com.vividsolutions.jts.geom.*;
 import org.heigit.bigspatialdata.oshdb.util.BoundingBox;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Predicate;
 
 /**
@@ -13,8 +14,24 @@ import java.util.function.Predicate;
  * https://blog.jochentopf.com/2017-02-06-expedicious-and-exact-extracts-with-osmium.html
  */
 public class FastBboxInPolygon extends FastInPolygon implements Predicate<BoundingBox> {
+  private Collection<Envelope> innerBboxes = new ArrayList<>();
+
   public <P extends Geometry & Polygonal> FastBboxInPolygon(P geom) {
     super(geom);
+
+    List<Polygon> polys = new LinkedList<>();
+    if (geom instanceof Polygon) {
+      polys.add((Polygon)geom);
+    } else if (geom instanceof MultiPolygon) {
+      MultiPolygon mp = (MultiPolygon)geom;
+      for (int i=0; i<mp.getNumGeometries(); i++)
+        polys.add((Polygon)mp.getGeometryN(i));
+    }
+    for (Polygon poly : polys) {
+      for (int i=0; i<poly.getNumInteriorRing(); i++) {
+        innerBboxes.add(poly.getInteriorRingN(i).getEnvelopeInternal());
+      }
+    }
   }
 
   /**
@@ -30,9 +47,18 @@ public class FastBboxInPolygon extends FastInPolygon implements Predicate<Boundi
     Point p2 = g.getExteriorRing().getPointN(1);
     Point p3 = g.getExteriorRing().getPointN(2);
     Point p4 = g.getExteriorRing().getPointN(3);
-    return crossingNumber(p1, true) == crossingNumber(p2, true)
-        && crossingNumber(p3, true) == crossingNumber(p4, true)
-        && crossingNumber(p2, false) == crossingNumber(p3, false)
-        && crossingNumber(p4, false) == crossingNumber(p1, false);
+    if (crossingNumber(p1, true) != crossingNumber(p2, true) ||
+        crossingNumber(p3, true) != crossingNumber(p4, true) ||
+        crossingNumber(p2, false) != crossingNumber(p3, false) ||
+        crossingNumber(p4, false) != crossingNumber(p1, false)) {
+      return false; // at least one of the bbox'es edges crosses the polygon
+    }
+    for (Envelope innerBBox : innerBboxes) {
+      if (boundingBox.minLat <= innerBBox.getMinY() && boundingBox.maxLat >= innerBBox.getMaxY() &&
+          boundingBox.minLon <= innerBBox.getMinX() && boundingBox.maxLon >= innerBBox.getMaxX()) {
+        return false; // the bounding box fully covers at least one of the polygon's inner rings
+      }
+    }
+    return true;
   }
 }
---
+++ oshdb/src/test/java/org/heigit/bigspatialdata/oshdb/util/fip/FastBboxInPolygonTest.java
@@ -0,0 +1,126 @@
+package org.heigit.bigspatialdata.oshdb.util.fip;
+
+import com.vividsolutions.jts.geom.*;
+import org.heigit.bigspatialdata.oshdb.util.BoundingBox;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class FastBboxInPolygonTest {
+  @Test
+  public void testBboxInPolygon() {
+    Polygon p = FastPointInPolygonTest.createPolygon();
+    FastBboxInPolygon bip = new FastBboxInPolygon(p);
+
+    GeometryFactory gf = new GeometryFactory();
+
+    // inside
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-0.1,0.1)), true);
+    // partially inside
+    assertEquals(bip.test(new BoundingBox(-1.5,-0.4,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,1.4,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-1.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-0.1,1.1)), false);
+    // in concave part
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.9,-0.8)), true);
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,0.8,0.9)), true);
+    // in concave part, coordinates all inside
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.9,0.9)), false);
+    // outside poly's bbox
+    assertEquals(bip.test(new BoundingBox(1.4,1.6,-0.1,0.1)), false);
+    // bbox covering
+    assertEquals(bip.test(new BoundingBox(-11,10,-10,10)), false);
+  }
+
+  @Test
+  public void testBboxInPolygonWithHole() {
+    Polygon p = FastPointInPolygonTest.createPolygonWithHole();
+    FastBboxInPolygon bip = new FastBboxInPolygon(p);
+
+    GeometryFactory gf = new GeometryFactory();
+
+    // inside
+    assertEquals(bip.test(new BoundingBox(2.1,2.2,-0.1,0.1)), true);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-0.9,-0.8)), true);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.8,0.9)), true);
+    assertEquals(bip.test(new BoundingBox(3.8,3.9,-0.1,0.1)), true);
+    // partially inside
+    assertEquals(bip.test(new BoundingBox(1.8,2.2,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-1.1,-0.8)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.8,1.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.8,4.1,-0.1,0.1)), false);
+    // in hole
+    assertEquals(bip.test(new BoundingBox(2.9,3.1,-0.1,0.1)), false);
+    // partially in hole
+    assertEquals(bip.test(new BoundingBox(2.4,2.6,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-0.6,-0.4)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.4,0.6)), false);
+    assertEquals(bip.test(new BoundingBox(3.4,3.6,-0.1,0.1)), false);
+    // intersecting hole
+    assertEquals(bip.test(new BoundingBox(2.9,3.1,-0.1,0.1)), false);
+    // outside poly's bbox
+    assertEquals(bip.test(new BoundingBox(4.1,4.2,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(1.8,1.9,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-1.2,-1.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,1.1,1.2)), false);
+    // covering hole, but all vertices inside polygon
+    assertEquals(bip.test(new BoundingBox(2.2,3.8,-0.8,0.8)), false);
+  }
+
+  @Test
+  public void testBboxInMultiPolygon() {
+    MultiPolygon p = FastPointInPolygonTest.createMultiPolygon();
+    FastBboxInPolygon bip = new FastBboxInPolygon(p);
+
+    GeometryFactory gf = new GeometryFactory();
+
+    // left polygon
+    // inside
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-0.1,0.1)), true);
+    // partially inside
+    assertEquals(bip.test(new BoundingBox(-1.5,-0.4,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,1.4,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-1.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(-0.6,-0.4,-0.1,1.1)), false);
+    // in concave part
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.9,-0.8)), true);
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,0.8,0.9)), true);
+    // in concave part, coordinates all inside
+    assertEquals(bip.test(new BoundingBox(0.4,0.6,-0.9,0.9)), false);
+    // outside poly's bbox
+    assertEquals(bip.test(new BoundingBox(1.4,1.6,-0.1,0.1)), false);
+    // bbox covering
+    assertEquals(bip.test(new BoundingBox(-11,10,-10,10)), false);
+
+    // right polygon
+    // inside
+    assertEquals(bip.test(new BoundingBox(2.1,2.2,-0.1,0.1)), true);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-0.9,-0.8)), true);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.8,0.9)), true);
+    assertEquals(bip.test(new BoundingBox(3.8,3.9,-0.1,0.1)), true);
+    // partially inside
+    assertEquals(bip.test(new BoundingBox(1.8,2.2,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-1.1,-0.8)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.8,1.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.8,4.1,-0.1,0.1)), false);
+    // in hole
+    assertEquals(bip.test(new BoundingBox(2.9,3.1,-0.1,0.1)), false);
+    // partially in hole
+    assertEquals(bip.test(new BoundingBox(2.4,2.6,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-0.6,-0.4)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,0.4,0.6)), false);
+    assertEquals(bip.test(new BoundingBox(3.4,3.6,-0.1,0.1)), false);
+    // intersecting hole
+    assertEquals(bip.test(new BoundingBox(2.9,3.1,-0.1,0.1)), false);
+    // outside poly's bbox
+    assertEquals(bip.test(new BoundingBox(4.1,4.2,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(1.8,1.9,-0.1,0.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,-1.2,-1.1)), false);
+    assertEquals(bip.test(new BoundingBox(3.1,3.2,1.1,1.2)), false);
+    // covering hole, but all vertices inside polygon
+    assertEquals(bip.test(new BoundingBox(2.2,3.8,-0.8,0.8)), false);
+  }
+}
