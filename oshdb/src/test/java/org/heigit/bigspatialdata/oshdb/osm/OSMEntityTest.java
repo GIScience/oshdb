ad9a28aaf5f8e66635cafda1a2171fe7dff8a98e
Martin Raifer <martin.raifer@uni-heidelberg.de>
2018-01-26 10:41:30 +0100

    add missing dependency on h2 (for OSHDB2H2Handler), fix tests

--- oshdb/src/test/java/org/heigit/bigspatialdata/oshdb/osm/OSMEntityTest.java
+++ oshdb/src/test/java/org/heigit/bigspatialdata/oshdb/osm/OSMEntityTest.java
@@ -12,7 +12,7 @@ public class OSMEntityTest {
     int[] properties = {1, 2};
     OSMNode instance = new OSMNode(1L, 1, new OSHDBTimestamp(1L), 1L, 1, properties, 1000000000L, 1000000000L);
     String expResult =
-        "NODE: ID:1 V:+1+ TS:1 CS:1 VIS:true UID:1 TAGS:[1, 2] 100.000000:100.000000";
+        "NODE: ID:1 V:+1+ TS:1 CS:1 VIS:true UID:1 TAGS:[1, 2] 100.0000000:100.0000000";
     String result = instance.toString();
     assertEquals(expResult, result);
   }
