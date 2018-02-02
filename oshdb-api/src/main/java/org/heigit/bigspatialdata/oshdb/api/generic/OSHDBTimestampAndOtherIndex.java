301dbb658fa268b47ce8426cf3e3d5d0ab3f710c
Martin Raifer <martin.raifer@uni-heidelberg.de>
2018-02-02 19:01:40 +0100

    add toString method to OSHDBTimestampAndOtherIndex class

--- oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/generic/OSHDBTimestampAndOtherIndex.java
+++ oshdb-api/src/main/java/org/heigit/bigspatialdata/oshdb/api/generic/OSHDBTimestampAndOtherIndex.java
@@ -9,17 +9,26 @@ import java.io.Serializable;
 
 public class OSHDBTimestampAndOtherIndex<V> implements Comparable<OSHDBTimestampAndOtherIndex>, Serializable {
   private Pair<V, OSHDBTimestamp> _payload;
+
   public OSHDBTimestampAndOtherIndex(OSHDBTimestamp timeIndex, V otherIndex) {
     this._payload = new ImmutablePair<>(otherIndex, timeIndex);
   }
+
   public OSHDBTimestamp getTimeIndex() {
     return this._payload.getRight();
   }
+
   public V getOtherIndex() {
     return this._payload.getLeft();
   }
+
   @Override
   public int compareTo(@NotNull OSHDBTimestampAndOtherIndex o) {
     return this._payload.compareTo(o._payload);
+  }
+
+  @Override
+  public String toString() {
+    return this.getOtherIndex().toString() + "@" + this.getTimeIndex().toString();
   }
 }
