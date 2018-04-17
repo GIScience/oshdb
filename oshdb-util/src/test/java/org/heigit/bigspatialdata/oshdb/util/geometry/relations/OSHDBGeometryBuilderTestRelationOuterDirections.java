e518ea76adea7676442c1983bf2de6a9162b62d3
Katharina Przybill <ob437@stud.uni-heidelberg.de>
2018-04-17 19:31:01 +0200

    add 3 other possible test cases to OSHDBGeometryBuilderTestRelationOuterDirections, add necessary test data

--- oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/relations/OSHDBGeometryBuilderTestRelationOuterDirections.java
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/relations/OSHDBGeometryBuilderTestRelationOuterDirections.java
@@ -52,5 +52,57 @@ public class OSHDBGeometryBuilderTestRelationOuterDirections {
     assertEquals(expectedPolygon.getArea(), intersection.getArea(), DELTA);
   }
 
-  //todo: all other combinations as well
+  @Test
+  public void test2() throws ParseException {
+    // end of partial ring matches end of current line
+    // to one point in outer ring two ways are pointing from different directions
+    OSMEntity entity = testData.relations().get(2L).get(0);
+    Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, tagInterpreter);
+    assertTrue(result instanceof Polygon);
+    assertTrue(result.isValid());
+    assertEquals(0, ((Polygon) result).getNumInteriorRing());
+
+    // compare if coordinates of created points equals the coordinates of polygon
+    Geometry expectedPolygon = (new WKTReader()).read(
+        "MULTIPOLYGON(((7.16 1.36,7.16 1.35,7.15 1.34,7.14 1.34,7.14 1.35,7.14 1.36,7.15 1.36,7.15 1.37,7.16 1.37,7.16 1.36)))"
+    );
+    Geometry intersection = result.intersection(expectedPolygon);
+    assertEquals(expectedPolygon.getArea(), intersection.getArea(), DELTA);
+  }
+
+  @Test
+  public void test3() throws ParseException {
+    // start of partial ring matches end of current line
+    // to one point in outer ring two ways are pointing from different directions
+    OSMEntity entity = testData.relations().get(3L).get(0);
+    Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, tagInterpreter);
+    assertTrue(result instanceof Polygon);
+    assertTrue(result.isValid());
+    assertEquals(0, ((Polygon) result).getNumInteriorRing());
+
+    // compare if coordinates of created points equals the coordinates of polygon
+    Geometry expectedPolygon = (new WKTReader()).read(
+        "MULTIPOLYGON(((7.16 1.36,7.16 1.35,7.15 1.34,7.14 1.34,7.14 1.35,7.14 1.36,7.15 1.36,7.15 1.37,7.16 1.37,7.16 1.36)))"
+    );
+    Geometry intersection = result.intersection(expectedPolygon);
+    assertEquals(expectedPolygon.getArea(), intersection.getArea(), DELTA);
+  }
+
+  @Test
+  public void test4() throws ParseException {
+    // end of partial ring matches to start of current line
+    // to one point in outer ring two ways are pointing from different directions
+    OSMEntity entity = testData.relations().get(4L).get(0);
+    Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, tagInterpreter);
+    assertTrue(result instanceof Polygon);
+    assertTrue(result.isValid());
+    assertEquals(0, ((Polygon) result).getNumInteriorRing());
+
+    // compare if coordinates of created points equals the coordinates of polygon
+    Geometry expectedPolygon = (new WKTReader()).read(
+        "MULTIPOLYGON(((7.16 1.36,7.16 1.35,7.15 1.34,7.14 1.34,7.14 1.35,7.14 1.36,7.15 1.36,7.15 1.37,7.16 1.37,7.16 1.36)))"
+    );
+    Geometry intersection = result.intersection(expectedPolygon);
+    assertEquals(expectedPolygon.getArea(), intersection.getArea(), DELTA);
+  }
 }
