6bf76a43081e2876b61dee09756c1e7759e4e6ed
Katharina Przybill <ob437@stud.uni-heidelberg.de>
2018-04-19 16:12:43 +0200

    delete prints

--- oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/osmhistorytestdata/OSHDBGeometryBuilderTestOsmHistoryTestDataNodes.java
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/osmhistorytestdata/OSHDBGeometryBuilderTestOsmHistoryTestDataNodes.java
@@ -43,6 +43,7 @@ public class OSHDBGeometryBuilderTestOsmHistoryTestDataNodes {
     // A single node, lat lon changed over time
     OSMEntity entity = testData.nodes().get(1L).get(0);
     // timestamp before oldest timestamp
+    // todo https://gitlab.gistools.geog.uni-heidelberg.de/giscience/big-data/ohsome/oshdb/issues/141
     OSHDBTimestamp timestamp_before =  TimestampParser.toOSHDBTimestamp("2007-01-01T00:00:00Z");
     Geometry result_before = OSHDBGeometryBuilder.getGeometry(entity, timestamp_before, areaDecider);
     assertTrue(result_before instanceof Point);
@@ -80,24 +81,19 @@ public class OSHDBGeometryBuilderTestOsmHistoryTestDataNodes {
   public void test2() {
     // A single node, tags changed over time
     OSMEntity entity = testData.nodes().get(2L).get(0);
-    System.out.println(entity);
     OSHDBTimestamp timestamp = entity.getTimestamp();
     Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, areaDecider);
     assertTrue(result instanceof Point);
     assertEquals(1.43, ((Point) result).getX(), DELTA);
     assertEquals(1.24, ((Point) result).getY(), DELTA);
     OSMEntity entity2 = testData.nodes().get(2L).get(1);
-    System.out.println(entity2);
     OSHDBTimestamp timestamp2 = entity2.getTimestamp();
-    System.out.println(timestamp2);
     Geometry result2 = OSHDBGeometryBuilder.getGeometry(entity2, timestamp2, areaDecider);
     assertTrue(result2 instanceof Point);
     assertEquals(1.43, ((Point) result2).getX(), DELTA);
     assertEquals(1.24, ((Point) result2).getY(), DELTA);
     OSMEntity entity3 = testData.nodes().get(2L).get(2);
-    System.out.println(entity3);
     OSHDBTimestamp timestamp3 = entity3.getTimestamp();
-    System.out.println(timestamp3);
     Geometry result3 = OSHDBGeometryBuilder.getGeometry(entity3, timestamp3, areaDecider);
     assertTrue(result3 instanceof Point);
     assertEquals(1.43, ((Point) result3).getX(), DELTA);
@@ -109,38 +105,29 @@ public class OSHDBGeometryBuilderTestOsmHistoryTestDataNodes {
     // A single node, visible changes
     // todo https://gitlab.gistools.geog.uni-heidelberg.de/giscience/big-data/ohsome/oshdb/issues/141
     OSMEntity entity = testData.nodes().get(3L).get(0);
-    System.out.println(entity);
     OSHDBTimestamp timestamp = entity.getTimestamp();
     Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, areaDecider);
     assertTrue(result instanceof Point);
     assertEquals(1.44, ((Point) result).getX(), DELTA);
     assertEquals(1.24, ((Point) result).getY(), DELTA);
     OSMEntity entity2 = testData.nodes().get(3L).get(1);
-    System.out.println(entity2);
     OSHDBTimestamp timestamp2 = entity2.getTimestamp();
-    System.out.println(timestamp2);
     Geometry result2 = OSHDBGeometryBuilder.getGeometry(entity2, timestamp2, areaDecider);
     assertTrue(result2 instanceof Point);
     assertTrue(result2.isEmpty());
     OSMEntity entity3 = testData.nodes().get(3L).get(2);
-    System.out.println(entity3);
     OSHDBTimestamp timestamp3 = entity3.getTimestamp();
-    System.out.println(timestamp3);
     Geometry result3 = OSHDBGeometryBuilder.getGeometry(entity3, timestamp3, areaDecider);
     assertTrue(result3 instanceof Point);
     assertEquals(1.44, ((Point) result3).getX(), DELTA);
     assertEquals(1.24, ((Point) result3).getY(), DELTA);
     OSMEntity entity4 = testData.nodes().get(3L).get(3);
-    System.out.println(entity4);
     OSHDBTimestamp timestamp4 = entity4.getTimestamp();
-    System.out.println(timestamp4);
     Geometry result4 = OSHDBGeometryBuilder.getGeometry(entity4, timestamp4, areaDecider);
     assertTrue(result4 instanceof Point);
     assertTrue(result4.isEmpty());
     OSMEntity entity5 = testData.nodes().get(3L).get(4);
-    System.out.println(entity5);
     OSHDBTimestamp timestamp5 = entity5.getTimestamp();
-    System.out.println(timestamp5);
     Geometry result5 = OSHDBGeometryBuilder.getGeometry(entity5, timestamp5, areaDecider);
     assertTrue(result5 instanceof Point);
     assertEquals(1.44, ((Point) result5).getX(), DELTA);
@@ -152,47 +139,36 @@ public class OSHDBGeometryBuilderTestOsmHistoryTestDataNodes {
     // A single node, various changes over time
     // todo https://gitlab.gistools.geog.uni-heidelberg.de/giscience/big-data/ohsome/oshdb/issues/141
     OSMEntity entity = testData.nodes().get(4L).get(0);
-    System.out.println(entity);
     OSHDBTimestamp timestamp = entity.getTimestamp();
     Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, areaDecider);
     assertTrue(result instanceof Point);
     assertEquals(1.42, ((Point) result).getX(), DELTA);
     assertEquals(1.21, ((Point) result).getY(), DELTA);
     OSMEntity entity2 = testData.nodes().get(4L).get(1);
-    System.out.println(entity2);
     OSHDBTimestamp timestamp2 = entity2.getTimestamp();
-    System.out.println(timestamp2);
     Geometry result2 = OSHDBGeometryBuilder.getGeometry(entity2, timestamp2, areaDecider);
     assertTrue(result2 instanceof Point);
     assertEquals(1.425, ((Point) result2).getX(), DELTA);
     assertEquals(1.20, ((Point) result2).getY(), DELTA);
     OSMEntity entity3 = testData.nodes().get(4L).get(2);
-    System.out.println(entity3);
     OSHDBTimestamp timestamp3 = entity3.getTimestamp();
-    System.out.println(timestamp3);
     Geometry result3 = OSHDBGeometryBuilder.getGeometry(entity3, timestamp3, areaDecider);
     assertTrue(result3 instanceof Point);
     assertTrue(result3.isEmpty());
     OSMEntity entity4 = testData.nodes().get(4L).get(3);
-    System.out.println(entity4);
     OSHDBTimestamp timestamp4 = entity4.getTimestamp();
-    System.out.println(timestamp4);
     Geometry result4 = OSHDBGeometryBuilder.getGeometry(entity4, timestamp4, areaDecider);
     assertTrue(result4 instanceof Point);
     assertEquals(1.42, ((Point) result4).getX(), DELTA);
     assertEquals(1.21, ((Point) result4).getY(), DELTA);
     OSMEntity entity5 = testData.nodes().get(4L).get(4);
-    System.out.println(entity5);
     OSHDBTimestamp timestamp5 = entity5.getTimestamp();
-    System.out.println(timestamp5);
     Geometry result5 = OSHDBGeometryBuilder.getGeometry(entity5, timestamp5, areaDecider);
     assertTrue(result5 instanceof Point);
     assertEquals(1.42, ((Point) result5).getX(), DELTA);
     assertEquals(1.215, ((Point) result5).getY(), DELTA);
     OSMEntity entity6 = testData.nodes().get(4L).get(5);
-    System.out.println(entity6);
     OSHDBTimestamp timestamp6 = entity6.getTimestamp();
-    System.out.println(timestamp6);
     Geometry result6 = OSHDBGeometryBuilder.getGeometry(entity6, timestamp6, areaDecider);
     assertTrue(result6 instanceof Point);
     assertEquals(1.42, ((Point) result6).getX(), DELTA);
