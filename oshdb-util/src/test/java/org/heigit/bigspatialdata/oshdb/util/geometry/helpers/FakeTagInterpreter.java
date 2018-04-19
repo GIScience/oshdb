44d34ee0288dd4d6427f33f54b82a5a3ddb6d6d9
Katharina Przybill <ob437@stud.uni-heidelberg.de>
2018-04-19 14:31:59 +0200

    add helpers

---
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/helpers/FakeTagInterpreter.java
@@ -0,0 +1,21 @@
+package org.heigit.bigspatialdata.oshdb.util.geometry.helpers;
+
+import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
+import org.heigit.bigspatialdata.oshdb.osm.OSMMember;
+import org.heigit.bigspatialdata.oshdb.osm.OSMRelation;
+import org.heigit.bigspatialdata.oshdb.util.tagInterpreter.TagInterpreter;
+
+public abstract class FakeTagInterpreter implements TagInterpreter {
+  @Override
+  public boolean isArea(OSMEntity entity) { return false; }
+  @Override
+  public boolean isLine(OSMEntity entity) { return false; }
+  @Override
+  public boolean hasInterestingTagKey(OSMEntity osm) { return false; }
+  @Override
+  public boolean isMultipolygonOuterMember(OSMMember osmMember) { return false; }
+  @Override
+  public boolean isMultipolygonInnerMember(OSMMember osmMember) { return false; }
+  @Override
+  public boolean isOldStyleMultipolygon(OSMRelation osmRelation) { return false; }
+}
---
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/helpers/OSMXmlReaderTagInterpreter.java
@@ -0,0 +1,49 @@
+package org.heigit.bigspatialdata.oshdb.util.geometry.helpers;
+
+import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
+import org.heigit.bigspatialdata.oshdb.osm.OSMMember;
+import org.heigit.bigspatialdata.oshdb.osm.OSMRelation;
+import org.heigit.bigspatialdata.oshdb.osm.OSMWay;
+import org.heigit.bigspatialdata.oshdb.util.test.OSMXmlReader;
+
+public class OSMXmlReaderTagInterpreter extends FakeTagInterpreter {
+
+  private int area;
+  private int areaYes;
+  private int type;
+  private int typeMultipolygon;
+  private int outer;
+  private int inner;
+
+  public OSMXmlReaderTagInterpreter(OSMXmlReader osmXmlReader) {
+    area = osmXmlReader.keys().getOrDefault("area", -1);
+    areaYes = area == -1 ? -1 : osmXmlReader.keyValues().get(area).getOrDefault("yes", -1);
+    type = osmXmlReader.keys().getOrDefault("type", -1);
+    typeMultipolygon = type == -1 ? -1 : osmXmlReader.keyValues().get(type).getOrDefault("multipolygon", -1);
+    outer = osmXmlReader.roles().getOrDefault("outer", -1);
+    inner = osmXmlReader.roles().getOrDefault("inner", -1);
+  }
+
+  @Override
+  public boolean isArea(OSMEntity e) {
+    if (e instanceof OSMWay) {
+      OSMMember[] nds = ((OSMWay) e).getRefs();
+      return nds.length >= 4 && nds[0].getId() == nds[nds.length - 1].getId() &&
+          e.hasTagValue(area, areaYes);
+    }
+    if (e instanceof OSMRelation) {
+      return e.hasTagValue(type, typeMultipolygon);
+    }
+    return true;
+  }
+
+  @Override
+  public boolean isMultipolygonOuterMember(OSMMember osmMember) {
+    return osmMember.getRawRoleId() == outer;
+  }
+
+  @Override
+  public boolean isMultipolygonInnerMember(OSMMember osmMember) {
+    return osmMember.getRawRoleId() == inner;
+  }
+}
