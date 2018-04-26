b2a7bb1c61ab3371c0705e36efc8fc95ab5aaf1e
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2018-04-26 16:20:39 +0200

    Remove unused loggers and unused imports

--- oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/bitset/LongBitSet.java
+++ oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/util/bitset/LongBitSet.java
@@ -1,11 +1,5 @@
 package org.heigit.bigspatialdata.oshdb.util.bitset;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.roaringbitmap.RoaringBitmap;
-import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
-
 /**
  * 
  * The idea come from 
