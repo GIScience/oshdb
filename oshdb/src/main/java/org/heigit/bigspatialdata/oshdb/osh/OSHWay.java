e583c85e8d93c2a5adcb9b38eb1917d0330dd958
Sascha Fendrich <sascha.fendrich@uni-heidelberg.de>
2018-04-26 17:08:21 +0200

    Remove unused loggers and unused imports

--- oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/osh/OSHWay.java
+++ oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/osh/OSHWay.java
@@ -12,18 +12,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
 import org.heigit.bigspatialdata.oshdb.osm.OSMMember;
 import org.heigit.bigspatialdata.oshdb.osm.OSMNode;
