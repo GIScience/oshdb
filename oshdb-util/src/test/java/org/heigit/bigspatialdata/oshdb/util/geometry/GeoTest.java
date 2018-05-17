e241b652a31d1f57e08fc65fa005c18e3054a103
Christina <christina.ludwig@uni-heidelberg.de>
2018-05-17 11:40:21 +0200

    adjust isWIthinDistance to earthRadius

--- oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/GeoTest.java
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/GeoTest.java
@@ -10,14 +10,12 @@ import org.junit.Test;
 
 public class GeoTest {
 
-    private static final double ONE_DEGREE_IN_METERS_AT_EQUATOR = 110567.;
-
     @Test
     public void TestIsWithinDistance() {
 
         Geometry geom1 = new GeometryFactory().createPoint(new Coordinate(48, 9)).buffer(1);
         Geometry geom2 = new GeometryFactory().createPoint(new Coordinate(45, 9)).buffer(1);
-        double distanceInMeter = 1. * ONE_DEGREE_IN_METERS_AT_EQUATOR;
+        double distanceInMeter = 1. * Geo.ONE_DEGREE_IN_METERS_AT_EQUATOR;
 
         assertTrue(Geo.isWithinDistance(geom1, geom2, distanceInMeter));
         assertTrue(!Geo.isWithinDistance(geom1, geom2, distanceInMeter/2.));
@@ -25,8 +23,8 @@ public class GeoTest {
 
     @Test
     public void TestConvertMetricDistanceToDegreeLongitude() {
-        assertEquals(1., Geo.convertMetricDistanceToDegreeLongitude(0, ONE_DEGREE_IN_METERS_AT_EQUATOR), 0.001 );
-        assertEquals(1., Geo.convertMetricDistanceToDegreeLongitude(48, 74000.), 0.001);
+        assertEquals(1., Geo.convertMetricDistanceToDegreeLongitude(0, Geo.ONE_DEGREE_IN_METERS_AT_EQUATOR), 0.01 );
+        assertEquals(1., Geo.convertMetricDistanceToDegreeLongitude(48, 74000.), 0.01);
     }
 
 }
