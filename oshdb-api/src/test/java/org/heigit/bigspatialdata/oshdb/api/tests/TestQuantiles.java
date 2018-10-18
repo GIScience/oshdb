c38d6f7414dbf97cdc14f0230122583302415a59
Martin Raifer <martin.raifer@uni-heidelberg.de>
2018-10-18 17:47:12 +0200

    add tests for MapAggregator quantile functions

--- oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestQuantiles.java
+++ oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestQuantiles.java
@@ -8,28 +8,19 @@ package org.heigit.bigspatialdata.oshdb.api.tests;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.SortedMap;
 import java.util.function.DoubleUnaryOperator;
-import java.util.stream.IntStream;
 import org.heigit.bigspatialdata.oshdb.api.db.OSHDBDatabase;
 import org.heigit.bigspatialdata.oshdb.api.db.OSHDBH2;
-import org.heigit.bigspatialdata.oshdb.api.generic.WeightedValue;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapAggregator;
 import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapReducer;
-import org.heigit.bigspatialdata.oshdb.api.mapreducer.OSMContributionView;
 import org.heigit.bigspatialdata.oshdb.api.mapreducer.OSMEntitySnapshotView;
-import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
 import org.heigit.bigspatialdata.oshdb.api.object.OSMEntitySnapshot;
 import org.heigit.bigspatialdata.oshdb.osm.OSMType;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBBoundingBox;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
-import org.heigit.bigspatialdata.oshdb.util.celliterator.ContributionType;
 import org.heigit.bigspatialdata.oshdb.util.time.OSHDBTimestamps;
 import org.junit.Test;
 
@@ -42,20 +33,11 @@ public class TestQuantiles {
   private final OSHDBBoundingBox bbox = new OSHDBBoundingBox(8.651133,49.387611,8.6561,49.390513);
   private final OSHDBTimestamps timestamps1 = new OSHDBTimestamps("2015-01-01");
   private final OSHDBTimestamps timestamps2 = new OSHDBTimestamps("2014-01-01", "2015-01-01");
-  private final OSHDBTimestamps timestamps72 = new OSHDBTimestamps("2010-01-01", "2015-12-01", OSHDBTimestamps.Interval.MONTHLY);
 
   private final double REQUIRED_ACCURACY = 1E-4;
 
   public TestQuantiles() throws Exception {
     oshdb = new OSHDBH2("./src/test/resources/test-data");
-  }
-
-  private MapReducer<OSMEntitySnapshot> createMapReducer() {
-    return OSMEntitySnapshotView.on(oshdb)
-        .timestamps(timestamps1)
-        .osmType(OSMType.WAY)
-        .osmTag("building", "yes")
-        .areaOfInterest(bbox);
   }
 
   private void assertApproximateQuantiles(
@@ -72,6 +54,16 @@ public class TestQuantiles {
     );
 
     assertEquals(expectedResult, result, expectedResult * REQUIRED_ACCURACY);
+  }
+
+  // MapReducer
+
+  private MapReducer<OSMEntitySnapshot> createMapReducer() {
+    return OSMEntitySnapshotView.on(oshdb)
+        .timestamps(timestamps1)
+        .osmType(OSMType.WAY)
+        .osmTag("building", "yes")
+        .areaOfInterest(bbox);
   }
 
   @Test
@@ -122,6 +114,87 @@ public class TestQuantiles {
     for (Double q : qs) {
       assertApproximateQuantiles(fullResult, q, quantilesFunction.applyAsDouble(q));
     }
+  }
+
+  // MapAggregator
+
+  private MapAggregator<OSHDBTimestamp, OSMEntitySnapshot> createMapAggregator() {
+    return OSMEntitySnapshotView.on(oshdb)
+        .timestamps(timestamps2)
+        .osmType(OSMType.WAY)
+        .osmTag("building", "yes")
+        .areaOfInterest(bbox)
+        .aggregateByTimestamp();
+  }
+
+  @Test
+  public void testMedianMapAggregator() throws Exception {
+    MapAggregator<OSHDBTimestamp, Integer> mr = this.createMapAggregator()
+        .map(s -> s.getGeometry().getCoordinates().length);
+    SortedMap<OSHDBTimestamp, List<Integer>> fullResult = mr.collect();
+    fullResult.values().forEach(Collections::sort);
+
+    SortedMap<OSHDBTimestamp, Double> medians = mr.quantile(0.8);
+
+    medians.forEach((ts, median) ->
+        assertApproximateQuantiles(fullResult.get(ts), 0.8, median)
+    );
+  }
+
+  @Test
+  public void testQuantileMapAggregator() throws Exception {
+    MapAggregator<OSHDBTimestamp, Integer> mr = this.createMapAggregator()
+        .map(s -> s.getGeometry().getCoordinates().length);
+    SortedMap<OSHDBTimestamp, List<Integer>> fullResult = mr.collect();
+    fullResult.values().forEach(Collections::sort);
+
+    SortedMap<OSHDBTimestamp, Double> quantiles = mr.quantile(0.8);
+
+    quantiles.forEach((ts, quantile) ->
+        assertApproximateQuantiles(fullResult.get(ts), 0.8, quantile)
+    );
+  }
+
+  @Test
+  public void testQuantilesMapAggregator() throws Exception {
+    MapAggregator<OSHDBTimestamp, Integer> mr = this.createMapAggregator()
+        .map(s -> s.getGeometry().getCoordinates().length);
+    SortedMap<OSHDBTimestamp, List<Integer>> fullResult = mr.collect();
+    fullResult.values().forEach(Collections::sort);
+
+    List<Double> qs = Arrays.asList(0.0, 0.2, 0.4, 0.6, 0.8, 1.0);
+    SortedMap<OSHDBTimestamp, List<Double>> quantiless = mr.quantiles(qs);
+
+    quantiless.forEach((ts, quantiles) -> {
+      for (Double quantile : quantiles) {
+        assertApproximateQuantiles(
+            fullResult.get(ts),
+            qs.get(quantiles.indexOf(quantile)),
+            quantile
+        );
+      }
+    });
+  }
+
+  @Test
+  public void testQuantilesFunctionMapAggregator() throws Exception {
+    MapAggregator<OSHDBTimestamp, Integer> mr = this.createMapAggregator()
+        .map(s -> s.getGeometry().getCoordinates().length);
+    SortedMap<OSHDBTimestamp, List<Integer>> fullResult = mr.collect();
+    fullResult.values().forEach(Collections::sort);
+
+    List<Double> qs = Arrays.asList(0.0, 0.2, 0.4, 0.6, 0.8, 1.0);
+    SortedMap<OSHDBTimestamp, DoubleUnaryOperator> quantilesFunctions = mr.quantiles();
+
+    quantilesFunctions.forEach((ts, quantilesFunction) -> {
+      for (Double q : qs) {
+        assertApproximateQuantiles(
+            fullResult.get(ts),
+            q,
+            quantilesFunction.applyAsDouble(q)
+        );
+      }
+    });
   }
 
 }
