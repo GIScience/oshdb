4da5303f57f35fc379592086d4c1c86a2563d557
Alexandra Beikert <beikert@heigit.org>
2019-01-15 15:24:09 +0100

    update zGrid: separate Space into SpaceLon and SpaceLat, delete interator and test

--- oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/index/zfc/ZGrid.java
+++ oshdb/src/main/java/org/heigit/bigspatialdata/oshdb/index/zfc/ZGrid.java
@@ -1,19 +1,19 @@
 package org.heigit.bigspatialdata.oshdb.index.zfc;
 
 import java.util.Comparator;
-import java.util.Iterator;
-
 import org.heigit.bigspatialdata.oshdb.OSHDB;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBBoundingBox;
-import org.heigit.bigspatialdata.oshdb.util.OSHDBBoundingBox.OVERLAP;
 
 public class ZGrid {
+  
+  public static OSHDBBoundingBox WORLD = new OSHDBBoundingBox(-180.0, -90.0, 180.0, 180.0);
 
   private static final int DIMENSION = 2;
   private static long ZOOM_FACTOR = 1L << 56;
   private static long ID_MASK = 0x00FFFFFFFFFFFFFFL;
 
-  private static final long space = (long) (360.0 * OSHDB.GEOM_PRECISION_TO_LONG);
+  private static final long spaceLon = (long) (360.0 * OSHDB.GEOM_PRECISION_TO_LONG);
+  private static final long spaceLat = (long) (180.0 * OSHDB.GEOM_PRECISION_TO_LONG);
   private final int maxZoom;
   private static final OSHDBBoundingBox zeroBoundingBox = new OSHDBBoundingBox(0, 0, 0, 0);
 
@@ -46,20 +46,22 @@ public class ZGrid {
     minLat = normalizeLat(minLat);
     maxLat = normalizeLat(maxLat);
 
-    if (!validateLon(minLon) || !validateLon(maxLon)
-            || !validateLat(minLat) || !validateLat(maxLat)) {
+    if (!validate(minLon, spaceLon) || !validate(maxLon, spaceLon)
+            || !validate(minLat, spaceLat) || !validate(maxLat, spaceLat)) {
+      // return value is deliberate '-1'
       return -1;
     }
 
-    int zoom = Math.min(optimalZoom(maxLon - minLon), optimalZoom(maxLat - minLat));
+    int zoom = Math.min(optimalZoom(maxLon - minLon, spaceLon), optimalZoom(maxLat - minLat, spaceLat));
     long zoomPow = (long) Math.pow(2, zoom);
 
     while (zoom > 0) {
-      long cellWidth = space / zoomPow;
-      x[0] = minLon / cellWidth;
-      x[1] = maxLon / cellWidth;
-      y[0] = minLat / cellWidth;
-      y[1] = maxLat / cellWidth;
+      long cellWidthLon = spaceLon / zoomPow;
+      long cellWidthLat = spaceLat / zoomPow;
+      x[0] = minLon / cellWidthLon;
+      x[1] = maxLon / cellWidthLon;
+      y[0] = minLat / cellWidthLat;
+      y[1] = maxLat / cellWidthLat;
 
       if (x[0] == x[1] && y[0] == y[1]) {
         break;
@@ -110,21 +112,6 @@ public class ZGrid {
     return 0;
   }
 
-  public Iterable<Long> iterableDF(OSHDBBoundingBox search) {
-    final ZGrid zGrid = this;
-    return new Iterable<Long>() {
-      final ZGrid grid = zGrid;
-
-      @Override
-      public Iterator<Long> iterator() {
-        return grid.iteratorDF(search);
-      }
-    };
-  }
-
-  public Iterator<Long> iteratorDF(OSHDBBoundingBox search) {
-    return new DFIterator(search, maxZoom, space);
-  }
 
   public static OSHDBBoundingBox getBoundingBox(long zId) {
     if (zId < 0) {
@@ -132,14 +119,15 @@ public class ZGrid {
     }
     final long id = getIdWithoutZoom(zId);
     final int zoom = getZoom(zId);
-    final long cellWidth = space / (long) Math.pow(2, zoom);
+    final long cellWidthLon = spaceLon / (long) Math.pow(2, zoom);
+    final long cellWidthLat = spaceLat / (long) Math.pow(2, zoom);
 
     long[] xy = getXY(id);
 
-    final long minLon = denormalizeLon(xy[0] * cellWidth);
-    final long maxLon = minLon + cellWidth - 1;
-    final long minLat = denormalizeLat(xy[1] * cellWidth);
-    final long maxLat = minLat + cellWidth - 1;
+    final long minLon = denormalizeLon(xy[0] * cellWidthLon);
+    final long maxLon = minLon + cellWidthLon - 1;
+    final long minLat = denormalizeLat(xy[1] * cellWidthLat);
+    final long maxLat = minLat + cellWidthLat - 1;
 
     return new OSHDBBoundingBox(minLon, minLat, maxLon, maxLat);
   }
@@ -153,7 +141,7 @@ public class ZGrid {
     return xy;
   }
 
-  private int optimalZoom(long delta) {
+  private int optimalZoom(long delta, long space) {
     if (delta == 0) {
       return maxZoom;
     }
@@ -197,30 +185,25 @@ public class ZGrid {
     return lat - (long) (90.0 * OSHDB.GEOM_PRECISION_TO_LONG);
   }
 
-  private static boolean validateLon(long lon) {
+  private static boolean validate(long lon, long space) {
     if (lon < 0 || lon > space) {
       return false;
     }
     return true;
   }
+  
 
-  private static boolean validateLat(long lat) {
-    if (lat < 0 || lat > space / 2) {
-      return false;
-    }
-    return true;
-  }
-
+  //TODO test comparator - works probably wrong
   public static final Comparator<Long> ORDER_DFS_TOP_DOWN = (a, b) -> {
     if(a == -1)
       return (b == -1)?0:-1;
     if(b == -1)
       return 1;
     
-    final long aZ = getZoom(a);
+    final long aZ = getZoom(a); 
     final long bZ = getZoom(b);
     if (aZ == bZ) {
-      return Long.compare(a, b);
+      return Long.compare(a, b); 
     }
     final long deltaZ = Math.abs(aZ - bZ);
     final long aId = getIdWithoutZoom(a);
@@ -265,113 +248,4 @@ public class ZGrid {
     return (r == 0) ? prio : r;
   };
 
-  public static OSHDBBoundingBox WORLD = new OSHDBBoundingBox(-180.0, -90.0, 180.0, 180.0);
-
-  private static class DFIterator implements Iterator<Long> {
-
-    private final OSHDBBoundingBox search;
-    private final int maxZoom;
-    private final State[] states;
-
-    private boolean hasNext = true;
-    private long next = 0;
-
-    private int z = 1;
-
-    public DFIterator(OSHDBBoundingBox search, int maxZoom, long space) {
-      this.search = search;
-      this.maxZoom = maxZoom;
-
-      this.states = new State[maxZoom + 1];
-      for (int z = 0; z < states.length; z++) {
-        State s = new State();
-        final long width = space / (long) Math.pow(2, z);
-        s.width = width;
-        s.bbox = new OSHDBBoundingBox(0L, 0L, width, width);
-        states[z] = s;
-      }
-
-    }
-
-    @Override
-    public boolean hasNext() {
-      return hasNext;
-    }
-
-    @Override
-    public Long next() {
-      long ret = next;
-      getNext();
-      return ret;
-    }
-
-    private void getNext() {
-      while (z > 0) {
-        final State state = states[z];
-        final int zoom = z;
-        final long id = state.id;
-
-        if (state.i > 3) {
-          state.i = 0;
-          state.complete = false;
-          z--;
-          continue;
-        }
-
-        final OSHDBBoundingBox.OVERLAP overlap;
-        if (states[z - 1].complete) {
-          overlap = OVERLAP.A_COMPLETE_IN_B;
-        } else {
-          OSHDBBoundingBox parentBBox = states[z - 1].bbox;
-          switch (state.i) {
-            case 0:
-              state.bbox = new OSHDBBoundingBox(parentBBox.getMinLonLong(), parentBBox.getMinLatLong(),
-                  parentBBox.getMinLonLong() + state.width, parentBBox.getMinLatLong() + state.width);
-              break;
-            case 1:
-              state.bbox = new OSHDBBoundingBox(parentBBox.getMinLonLong() + state.width, parentBBox.getMinLatLong(),
-                  parentBBox.getMinLonLong() + 2 * state.width, parentBBox.getMinLatLong() + state.width);
-              break;
-            case 2:
-              state.bbox = new OSHDBBoundingBox(parentBBox.getMinLonLong(), parentBBox.getMinLatLong() + state.width,
-                  parentBBox.getMinLonLong() + state.width, parentBBox.getMinLatLong() + 2 * state.width);
-              break;
-            case 3:
-              state.bbox = new OSHDBBoundingBox(parentBBox.getMinLonLong() + state.width, parentBBox.getMinLatLong() + state.width,
-                  parentBBox.getMinLonLong() + 2 * state.width, parentBBox.getMinLatLong() + 2 * state.width);
-          }
-          overlap = OSHDBBoundingBox.overlap(state.bbox, search);
-        }
-
-        if (overlap == OVERLAP.A_COMPLETE_IN_B) {
-          state.complete = true;
-        }
-
-        if (z < maxZoom && overlap != OVERLAP.NONE) {
-          z++;
-          states[z].id = state.id << 2;
-        }
-
-        state.i += 1;
-        state.id += 1;
-
-        if (overlap != OVERLAP.NONE) {
-          hasNext = true;
-          next = addZoomToId(id, zoom);
-          return;
-        }
-      }
-      hasNext = false;
-      next = -1;
-    }
-
-    private static class State {
-
-      private int i = 0;
-      private long id;
-      private OSHDBBoundingBox bbox;
-      private long width;
-      private boolean complete = false;
-    }
-  }
 }
