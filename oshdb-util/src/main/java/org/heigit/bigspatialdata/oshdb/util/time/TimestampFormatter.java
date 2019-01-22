dd95607d16fb5f5890c5a65ad4823523014bd9ff
Martin Raifer <martin.raifer@uni-heidelberg.de>
2019-01-22 17:59:22 +0100

    fill in missing javadoc entries

--- oshdb-util/src/main/java/org/heigit/bigspatialdata/oshdb/util/time/TimestampFormatter.java
+++ oshdb-util/src/main/java/org/heigit/bigspatialdata/oshdb/util/time/TimestampFormatter.java
@@ -1,6 +1,5 @@
 package org.heigit.bigspatialdata.oshdb.util.time;
 
-import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -12,13 +11,13 @@ import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
  */
 public class TimestampFormatter {
 
-  private static TimestampFormatter _instance;
-  private final ThreadLocal<SimpleDateFormat> _formatDate = ThreadLocal.withInitial(() -> {
+  private static TimestampFormatter instance;
+  private final ThreadLocal<SimpleDateFormat> formatDate = ThreadLocal.withInitial(() -> {
     SimpleDateFormat ret = new SimpleDateFormat("yyyy-MM-dd");
     ret.setTimeZone(TimeZone.getTimeZone("UTC"));
     return ret;
   });
-  private final ThreadLocal<SimpleDateFormat> _formatIsoDateTime = ThreadLocal.withInitial(() -> {
+  private final ThreadLocal<SimpleDateFormat> formatIsoDateTime = ThreadLocal.withInitial(() -> {
     SimpleDateFormat ret = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
     ret.setTimeZone(TimeZone.getTimeZone("UTC"));
     return ret;
@@ -27,51 +26,51 @@ public class TimestampFormatter {
   /**
    * Get a standard TimestampFormatter.
    *
-   * @return
+   * @return a timestamp formatter object
    */
   public static TimestampFormatter getInstance() {
-    if (_instance == null) {
-      _instance = new TimestampFormatter();
+    if (instance == null) {
+      instance = new TimestampFormatter();
     }
-    return _instance;
+    return instance;
   }
 
   /**
    * Converts a date to the format {@code yyyy-MM-dd}.
    *
-   * @param date
-   * @return
+   * @param date the date to be printed
+   * @return the formatted date string
    */
   public String date(Date date) {
-    return _formatDate.get().format(date);
+    return formatDate.get().format(date);
   }
 
   /**
    * Converts a date to the format {@code yyyy-MM-dd'T'HH:mm:ss'Z'}.
    *
-   * @param date
-   * @return
+   * @param date the date to be printed
+   * @return the formatted date string
    */
   public String isoDateTime(Date date) {
-    return _formatIsoDateTime.get().format(date);
+    return formatIsoDateTime.get().format(date);
   }
 
   /**
    * Converts a unix-timestamp (oshdb-timestamp) to the format
    * {@code yyyy-MM-dd'T'HH:mm:ss'Z'} (OSM-Timestamp).
    *
-   * @param timestamp
-   * @return
+   * @param timestamp the timestamp to be printed
+   * @return the formatted date string
    */
   public String isoDateTime(long timestamp) {
-    return _formatIsoDateTime.get().format(timestamp * 1000);
+    return formatIsoDateTime.get().format(timestamp * 1000);
   }
 
   /**
    * Converts a date to the format {@code yyyy-MM-dd'T'HH:mm:ss'Z'}.
    *
-   * @param date
-   * @return
+   * @param date the date to be printed
+   * @return the formatted date string
    */
   public String isoDateTime(OSHDBTimestamp date) {
     return this.isoDateTime(date.getRawUnixTimestamp());
