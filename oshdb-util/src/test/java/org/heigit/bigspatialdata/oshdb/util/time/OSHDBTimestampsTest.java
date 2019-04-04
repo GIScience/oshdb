dca064f7a3c0be60526d270bfc6083dbea5813e4
Johannes Visintini <johannes.visintini@uni-heidelberg.de>
2019-04-04 18:38:12 +0200

    Improve testTimeIntervals tests

--- oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/time/OSHDBTimestampsTest.java
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/time/OSHDBTimestampsTest.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.SortedSet;
-import org.heigit.bigspatialdata.oshdb.util.OSHDBTimestamp;
 import org.heigit.bigspatialdata.oshdb.util.time.OSHDBTimestamps.Interval;
 import org.junit.Test;
 
@@ -16,43 +14,63 @@ public class OSHDBTimestampsTest {
 
   @Test
   public void testTimeIntervals() {
-    List<List<Object>> inputs = new ArrayList<>();
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2010-01-31T12:34:56", Interval.YEARLY,
-        Arrays.asList("2008-01-31T12:34:56", "2009-01-31T12:34:56", "2010-01-31T12:34:56")
-    ));
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2008-07-31T12:34:56", Interval.QUARTERLY,
-        Arrays.asList( "2008-01-31T12:34:56", "2008-04-30T12:34:56", "2008-07-30T12:34:56")
-    ));
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2008-03-31T12:34:56", Interval.MONTHLY,
-        Arrays.asList( "2008-01-31T12:34:56", "2008-02-29T12:34:56", "2008-03-29T12:34:56")
-    ));
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2008-02-14T12:34:56", Interval.WEEKLY,
-        Arrays.asList( "2008-01-31T12:34:56", "2008-02-07T12:34:56", "2008-02-14T12:34:56")
-    ));
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2008-02-02T12:34:56", Interval.DAILY,
-        Arrays.asList( "2008-01-31T12:34:56", "2008-02-01T12:34:56", "2008-02-02T12:34:56")
-    ));
-    inputs.add(Arrays.asList("2008-01-31T12:34:56", "2008-01-31T15:00:00", Interval.HOURLY,
-        Arrays.asList( "2008-01-31T12:34:56", "2008-01-31T13:34:56", "2008-01-31T14:34:56")
-    ));
+    List<String> startList = new ArrayList<>();
+    List<String> endList = new ArrayList<>();
+    List<Interval> intervalList = new ArrayList<>();
+    List<List<String>> expectedResultList = new ArrayList<>();
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2010-01-31T12:34:56");
+    intervalList.add(Interval.YEARLY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2009-01-31T12:34:56", "2010-01-31T12:34:56"));
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2008-07-31T12:34:56");
+    intervalList.add(Interval.QUARTERLY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2008-04-30T12:34:56", "2008-07-30T12:34:56"));
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2008-03-31T12:34:56");
+    intervalList.add(Interval.MONTHLY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2008-02-29T12:34:56", "2008-03-29T12:34:56"));
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2008-02-14T12:34:56");
+    intervalList.add(Interval.WEEKLY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2008-02-07T12:34:56", "2008-02-14T12:34:56"));
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2008-02-02T12:34:56");
+    intervalList.add(Interval.DAILY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2008-02-01T12:34:56", "2008-02-02T12:34:56"));
+
+    startList.add("2008-01-31T12:34:56");
+    endList.add("2008-01-31T15:00:00");
+    intervalList.add(Interval.HOURLY);
+    expectedResultList.add(Arrays.asList("2008-01-31T12:34:56", "2008-01-31T13:34:56", "2008-01-31T14:34:56"));
+
+    // check if lists have the same size
+    assertEquals(startList.size(), endList.size());
+    assertEquals(startList.size(), intervalList.size());
+    assertEquals(startList.size(), expectedResultList.size());
 
     List<Interval> testedIntervals = new ArrayList<>();
-    for (List<Object> testobj : inputs) {
-      OSHDBTimestamps timestamps = new OSHDBTimestamps((String) testobj.get(0),
-          (String) testobj.get(1),
-          (Interval) testobj.get(2));
-      SortedSet<OSHDBTimestamp> result = timestamps.get();
-      Iterable<String> expResult = (Iterable<String>) testobj.get(3);
+    for (int i = 0; i < startList.size(); i++) {
+      OSHDBTimestamps timestamps = new OSHDBTimestamps(startList.get(i), endList.get(i),
+          intervalList.get(i));
 
-      Iterator resultIt = result.iterator();
-      Iterator expResultIt = expResult.iterator();
+      Iterator resultIt = timestamps.get().iterator();
+      Iterator expResultIt = expectedResultList.get(i).iterator();
+      // check if results are exactly the same
       while (resultIt.hasNext()) {
-        assertEquals(expResultIt.next(), resultIt.next().toString());
+        assertEquals(expResultIt.next().toString(), resultIt.next().toString());
       }
+      // check if more results are expected
       assertFalse(expResultIt.hasNext());
-      testedIntervals.add((Interval) testobj.get(2));
+      testedIntervals.add(intervalList.get(i));
     }
 
+    // check if all intervals in the enum are tested
     for (Interval interval : EnumSet.allOf(Interval.class)) {
       assertTrue(testedIntervals.contains(interval));
     }
