02e12bccb0d38d5058c719e415cb4936edf31c20
Martin Raifer <martin.raifer@uni-heidelberg.de>
2019-04-12 18:42:29 +0200

    drop unused import

--- oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestHelpersOSMContributionView.java
+++ oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestHelpersOSMContributionView.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.heigit.bigspatialdata.oshdb.api.tests;
 
-import java.util.List;
-import java.util.TreeSet;
 import org.heigit.bigspatialdata.oshdb.api.db.OSHDBDatabase;
 import org.heigit.bigspatialdata.oshdb.api.db.OSHDBH2;
 import org.heigit.bigspatialdata.oshdb.api.generic.WeightedValue;
