dfe94343bcf482ccc06badd46f67d2079eabaded
Martin Raifer <martin.raifer@heigit.org>
2020-05-22 11:47:48 +0200

    refactor to deduplicate code

--- src/main/java/org/heigit/ohsome/filter/TagFilterEqualsAnyOf.java
+++ src/main/java/org/heigit/ohsome/filter/TagFilterEqualsAnyOf.java
@@ -1,9 +1,6 @@
 package org.heigit.ohsome.filter;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import org.heigit.bigspatialdata.oshdb.osh.OSHEntity;
@@ -13,22 +10,9 @@ import org.heigit.bigspatialdata.oshdb.util.OSHDBTag;
 /**
  * A tag filter which executes a "key in (value1, value2, …)" check.
  */
-public class TagFilterEqualsAnyOf implements Filter {
-  private final Set<OSHDBTag> tags;
-  private final int keyId;
-
+public class TagFilterEqualsAnyOf extends TagFilterAnyOf {
   TagFilterEqualsAnyOf(@Nonnull Collection<OSHDBTag> tags) {
-    Optional<OSHDBTag> firstTag = tags.stream().findFirst();
-    if (!firstTag.isPresent()) {
-      throw new IllegalStateException("list of tags must not be empty in a key in (values) filter");
-    } else {
-      this.keyId = firstTag.get().getKey();
-      this.tags = new HashSet<>(tags);
-    }
-    if (!tags.stream().allMatch(tag -> tag.getKey() == this.keyId)) {
-      throw new IllegalStateException(
-          "list of tags must all share the same tag key in a key in (values) filter");
-    }
+    super(tags);
   }
 
   @Override
--- src/main/java/org/heigit/ohsome/filter/TagFilterNotEqualsAnyOf.java
+++ src/main/java/org/heigit/ohsome/filter/TagFilterNotEqualsAnyOf.java
@@ -1,9 +1,6 @@
 package org.heigit.ohsome.filter;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
@@ -12,22 +9,9 @@ import org.heigit.bigspatialdata.oshdb.util.OSHDBTag;
 /**
  * A tag filter which executes a "key not in (value1, value2, …)" check.
  */
-public class TagFilterNotEqualsAnyOf implements Filter {
-  private final Set<OSHDBTag> tags;
-  private final int keyId;
-
+public class TagFilterNotEqualsAnyOf extends TagFilterAnyOf {
   TagFilterNotEqualsAnyOf(@Nonnull Collection<OSHDBTag> tags) {
-    Optional<OSHDBTag> firstTag = tags.stream().findFirst();
-    if (!firstTag.isPresent()) {
-      throw new IllegalStateException("list of tags must not be empty in a key in (values) filter");
-    } else {
-      this.keyId = firstTag.get().getKey();
-      this.tags = new HashSet<>(tags);
-    }
-    if (!tags.stream().allMatch(tag -> tag.getKey() == this.keyId)) {
-      throw new IllegalStateException(
-          "list of tags must all share the same tag key in a key in (values) filter");
-    }
+    super(tags);
   }
 
   @Override
