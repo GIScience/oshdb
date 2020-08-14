27139259555bac7bda0d0ef73c66fe73737ba21a
Martin Raifer <martin.raifer@heigit.org>
2020-08-14 14:29:07 +0200

    test filter(FilterExpression) variant

--- oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestOhsomeFilter.java
+++ oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestOhsomeFilter.java
@@ -11,10 +11,12 @@ import org.heigit.bigspatialdata.oshdb.osm.OSMMember;
 import org.heigit.bigspatialdata.oshdb.osm.OSMRelation;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBBoundingBox;
 import org.heigit.bigspatialdata.oshdb.util.taginterpreter.TagInterpreter;
+import org.heigit.bigspatialdata.oshdb.util.tagtranslator.TagTranslator;
 import org.heigit.bigspatialdata.oshdb.util.time.OSHDBTimestamps;
 import org.heigit.bigspatialdata.oshdb.api.object.OSMContribution;
 import org.heigit.bigspatialdata.oshdb.osm.OSMType;
 import org.heigit.bigspatialdata.oshdb.util.celliterator.ContributionType;
+import org.heigit.ohsome.filter.FilterParser;
 import org.junit.Test;
 
 import java.util.Set;
@@ -32,11 +34,14 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestOhsomeFilter {
   private final OSHDBDatabase oshdb;
+  private final FilterParser filterParser;
 
   private final OSHDBBoundingBox bbox = new OSHDBBoundingBox(8.651133,49.387611,8.6561,49.390513);
 
   public TestOhsomeFilter() throws Exception {
-    oshdb = new OSHDBH2("./src/test/resources/test-data");
+    OSHDBH2 oshdb = new OSHDBH2("./src/test/resources/test-data");
+    filterParser = new FilterParser(new TagTranslator(oshdb.getConnection()));
+    this.oshdb = oshdb;
   }
 
   private MapReducer<OSMEntitySnapshot> createMapReducer() throws Exception {
@@ -56,6 +61,16 @@ public class TestOhsomeFilter {
   }
 
   @Test
+  public void testFilterObject() throws Exception {
+    MapReducer<OSMEntitySnapshot> mr = createMapReducer();
+    Number result = mr
+        .filter(filterParser.parse("type:way and geometry:polygon and building=*"))
+        .count();
+
+    assertEquals(42, result.intValue());
+  }
+
+  @Test
   public void testAggregateFilter() throws Exception {
     SortedMap<OSMType, Integer> result = createMapReducer()
         .aggregateBy(x -> x.getEntity().getType())
@@ -65,5 +80,16 @@ public class TestOhsomeFilter {
     assertEquals(2, result.entrySet().size());
     assertEquals(42, result.get(OSMType.WAY).intValue());
     assertEquals(1, result.get(OSMType.RELATION).intValue());
+  }
+
+  @Test
+  public void testAggregateFilterObject() throws Exception {
+    MapReducer<OSMEntitySnapshot> mr = createMapReducer();
+    SortedMap<OSMType, Integer> result = mr
+        .aggregateBy(x -> x.getEntity().getType())
+        .filter(filterParser.parse("(geometry:polygon or geometry:other) and building=*"))
+        .count();
+
+    assertEquals(42, result.get(OSMType.WAY).intValue());
   }
 }
