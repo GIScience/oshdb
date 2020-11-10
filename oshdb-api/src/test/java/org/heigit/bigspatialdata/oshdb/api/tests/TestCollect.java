b7079de839831deb66d448b54e4f7bf21db74a85
Johannes Visintini <johannes.visintini@heigit.org>
2020-11-10 19:20:22 +0100

    remove license header informations

--- oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestCollect.java
+++ oshdb-api/src/test/java/org/heigit/bigspatialdata/oshdb/api/tests/TestCollect.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.heigit.bigspatialdata.oshdb.api.tests;
 
 import static org.junit.Assert.assertEquals;
@@ -104,5 +98,4 @@ public class TestCollect {
         .get(Iterables.get(timestamps72.get(), 60))
         .size());
   }
-
 }
