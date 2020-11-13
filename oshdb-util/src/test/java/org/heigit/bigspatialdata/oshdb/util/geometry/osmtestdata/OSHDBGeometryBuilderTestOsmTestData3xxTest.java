01e3ee1a46ee8d405324e82effa8b8fbefe52501
Martin Raifer <martin.raifer@heigit.org>
2020-11-13 10:51:37 +0100

    slightly improve "3xx" tests:
    make "no exception" throwing tests more explicit by replacing try-catch->fail with `expected = Test.None.class)`,
    fix some tests and fill out some other ones

--- oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/osmtestdata/OSHDBGeometryBuilderTestOsmTestData3xxTest.java
+++ oshdb-util/src/test/java/org/heigit/bigspatialdata/oshdb/util/geometry/osmtestdata/OSHDBGeometryBuilderTestOsmTestData3xxTest.java
@@ -1,6 +1,6 @@
 package org.heigit.bigspatialdata.oshdb.util.geometry.osmtestdata;
 
-import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.heigit.bigspatialdata.oshdb.osm.OSMEntity;
@@ -25,79 +25,63 @@ public class OSHDBGeometryBuilderTestOsmTestData3xxTest {
     areaDecider = new OSMXmlReaderTagInterpreter(testData);
   }
 
+  private Geometry buildEntityGeometry(long id) {
+    OSMEntity entity = testData.nodes().get(id).get(0);
+    return OSHDBGeometryBuilder.getGeometry(entity, timestamp, areaDecider);
+  }
+
   @Test
   public void test300() {
     // Normal node with uid (and user name)
-    OSMEntity entity = testData.nodes().get(200000L).get(0);
-    Geometry result = OSHDBGeometryBuilder.getGeometry(entity, timestamp, areaDecider);
+    Geometry result = buildEntityGeometry(200000L);
     assertTrue(result instanceof Point);
-    Integer entityUid = testData.nodes().get(200000L).get(0).getUserId();
-    assertTrue(entityUid instanceof Integer);
+    int entityUid = testData.nodes().get(200000L).get(0).getUserId();
+    assertEquals(1, entityUid);
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void test301() {
+    // Empty username on node should not happen
+    buildEntityGeometry(201000L);
   }
 
   @Test
-  public void test301() {
-    // Empty username on node should not happen
-    OSMEntity entity1 = testData.nodes().get(201000L).get(0);
-    try {
-      OSHDBGeometryBuilder.getGeometry(entity1, timestamp, areaDecider);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail("Should not have thrown any exception");
-    }
-  }
-
-  /* @Test
   public void test302() {
     // No uid and no user name means user is anonymous
     // user name is not priority
-  } */
-
-  /* @Test
-  public void test303() {
-    // No uid and no user name means user is anonymous
-    // user name is not priority
-  } */
+    int entityUid = testData.nodes().get(202000L).get(0).getUserId();
+    assertTrue(entityUid < 1);
+  }
 
   @Test
+  public void test303() {
+    // uid 0 is the anonymous user
+    int entityUid = testData.nodes().get(203000L).get(0).getUserId();
+    assertEquals(0, entityUid);
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
   public void test304() {
     // negative user ids are not allowed (but -1 could have been meant as anonymous user)
-    OSMEntity entity1 = testData.nodes().get(204000L).get(0);
-    try {
-      OSHDBGeometryBuilder.getGeometry(entity1, timestamp, areaDecider);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail("Should not have thrown any exception");
-    }
+    buildEntityGeometry(204000L);
   }
 
-  @Test
+  @Test(expected = Test.None.class /* no exception expected */)
   public void test305() {
     // uid < 0 and username is inconsistent and definitely wrong
-    OSMEntity entity1 = testData.nodes().get(205000L).get(0);
-    try {
-      OSHDBGeometryBuilder.getGeometry(entity1, timestamp, areaDecider);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail("Should not have thrown any exception");
-    }
+    buildEntityGeometry(205000L);
   }
 
-  /* @Test
+  @Test(expected = Test.None.class /* no exception expected */)
   public void test306() {
     // 250 characters in username is okay
     // user name is not priority
-  } */
+    buildEntityGeometry(206000L);
+  }
 
-  @Test
+  @Test(expected = Test.None.class /* no exception expected */)
   public void test307() {
     // 260 characters in username is too long
-    OSMEntity entity1 = testData.nodes().get(207000L).get(0);
-    try {
-      OSHDBGeometryBuilder.getGeometry(entity1, timestamp, areaDecider);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail("Should not have thrown any exception");
-    }
+    buildEntityGeometry(207000L);
   }
 }
