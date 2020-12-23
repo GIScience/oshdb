550ac891cc30638f11554047b77144fb27402a7c
Martin Raifer <martin.raifer@heigit.org>
2020-12-23 12:01:31 +0100

    add inline comment, more consistent usage of Longs

--- oshdb/src/test/java/org/heigit/ohsome/oshdb/osh/OSHRelationTest.java
+++ oshdb/src/test/java/org/heigit/ohsome/oshdb/osh/OSHRelationTest.java
@@ -260,6 +260,9 @@ public class OSHRelationTest {
 
   @Test
   public void testIssue325() throws IOException {
+    // tests that the bug reported in https://github.com/GIScience/oshdb/issues/325 is fixed:
+    // relations referencing redacted ways caused a crash in the OSHEntities utility class
+    // when calculating the relation's modification timestamps
     OSHNode hnode1 = OSHNodeImpl.build(new ArrayList<>(List.of(
         new OSMNode(123L, 2, new OSHDBTimestamp(2L), 2L, 0, new int[]{}, 0, 0),
         new OSMNode(123L, 1, new OSHDBTimestamp(1L), 1L, 0, new int[]{}, 0, 0))));
@@ -269,20 +272,20 @@ public class OSHRelationTest {
 
     OSHWay hway1 = OSHWayImpl.build(new ArrayList<>(List.of(
         new OSMWay(1, 1, new OSHDBTimestamp(1L), 1L, 0, new int[]{}, new OSMMember[]{
-            new OSMMember(123, OSMType.NODE, 0),
-            new OSMMember(124, OSMType.NODE, 0)})
+            new OSMMember(123L, OSMType.NODE, 0),
+            new OSMMember(124L, OSMType.NODE, 0)})
     )), List.of(hnode1, hnode2));
 
     OSHWay hway2 = OSHWayImpl.build(new ArrayList<>(List.of(
-        new OSMWay(2, -4, new OSHDBTimestamp(9L), 9L, 9, new int[]{}, new OSMMember[]{})
+        new OSMWay(2L, -4, new OSHDBTimestamp(9L), 9L, 9, new int[]{}, new OSMMember[]{})
     )), Collections.emptyList());
 
     OSHRelation hrelation = OSHRelationImpl.build(new ArrayList<>(List.of(
-        new OSMRelation(1, 2, new OSHDBTimestamp(8L), 8L, 8, new int[]{1, 1, 2, 2}, new OSMMember[]{
-            new OSMMember(1, OSMType.WAY, 0),
-            new OSMMember(2, OSMType.WAY, 0)}),
-        new OSMRelation(1, 1, new OSHDBTimestamp(1L), 1L, 0, new int[]{1, 1, 2, 2}, new OSMMember[]{
-            new OSMMember(1, OSMType.WAY, 0)})
+        new OSMRelation(1L, 2, new OSHDBTimestamp(8L), 8L, 8, new int[]{1, 1, 2, 2}, new OSMMember[]{
+            new OSMMember(1L, OSMType.WAY, 0),
+            new OSMMember(2L, OSMType.WAY, 0)}),
+        new OSMRelation(1L, 1, new OSHDBTimestamp(1L), 1L, 0, new int[]{1, 1, 2, 2}, new OSMMember[]{
+            new OSMMember(1L, OSMType.WAY, 0)})
     )), List.of(hnode1, hnode2), List.of(hway1, hway2));
 
     List<OSHDBTimestamp> tss = OSHEntities.getModificationTimestamps(hrelation, true);
