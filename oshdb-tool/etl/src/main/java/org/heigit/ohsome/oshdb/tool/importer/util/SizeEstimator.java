ec1b59896cb3e49ca888a3c03788d37bb1505a5c
Martin Raifer <martin@raifer.tech>
2021-05-07 14:23:13 +0200

    make constants final and initialize them directly

--- oshdb-tool/etl/src/main/java/org/heigit/ohsome/oshdb/tool/importer/util/SizeEstimator.java
+++ oshdb-tool/etl/src/main/java/org/heigit/ohsome/oshdb/tool/importer/util/SizeEstimator.java
@@ -2,51 +2,35 @@ package org.heigit.ohsome.oshdb.tool.importer.util;
 
 public class SizeEstimator {
 
-  private static long OBJ_HEADER;
-  private static long ARR_HEADER;
-  private static long PADDING;
-  private static long INT_FIELD = 4;
-  private static long OBJ_REF;
-  private static long OBJ_OVERHEAD;
-  private static boolean IS_64_BIT_JVM;
+  // By default we assume 64 bit JVM (defensive approach since we will get larger estimations in
+  // case we are not sure)
+  // check the system property "sun.arch.data.model" not very safe, as it might not work for all
+  // JVM implementations, nevertheless the worst thing that might happen is that the JVM is 32bit
+  // but we assume its 64bit, so we will be counting a few extra bytes per string object no harm
+  // done here since this is just an approximation.
+  private static final boolean IS_64_BIT_JVM = System.getProperty("sun.arch.data.model") == null
+      || !System.getProperty("sun.arch.data.model").contains("32");
+
+  // The sizes below are a bit rough as we don't take into account
+  // advanced JVM options such as compressed oops
+  // however if our calculation is not accurate it'll be a bit over
+  // so there is no danger of an out of memory error because of this.
+  private static final long OBJ_HEADER = IS_64_BIT_JVM ? 16 : 8;
+  private static final long ARR_HEADER = IS_64_BIT_JVM ? 24 : 12;
+  private static final long OBJ_REF = IS_64_BIT_JVM ? 8 : 4;
+  private static final long PADDING = 4;
+  private static final long INT_FIELD = 4;
+  private static final long OBJ_OVERHEAD = OBJ_HEADER + OBJ_REF + INT_FIELD + ARR_HEADER + PADDING;
 
   private static final long AVL_ENTRY_SIZE = 96;
   private static final long AVL_MAP_SIZE = 128;
-
-  static {
-    // By default we assume 64 bit JVM
-    // (defensive approach since we will get
-    // larger estimations in case we are not sure)
-    IS_64_BIT_JVM = true;
-    // check the system property "sun.arch.data.model"
-    // not very safe, as it might not work for all JVM implementations
-    // nevertheless the worst thing that might happen is that the JVM is
-    // 32bit
-    // but we assume its 64bit, so we will be counting a few extra bytes per
-    // string object
-    // no harm done here since this is just an approximation.
-    String arch = System.getProperty("sun.arch.data.model");
-    if (arch != null && arch.contains("32")) {
-      // If exists and is 32 bit then we assume a 32bit JVM
-      IS_64_BIT_JVM = false;
-    }
-    // The sizes below are a bit rough as we don't take into account
-    // advanced JVM options such as compressed oops
-    // however if our calculation is not accurate it'll be a bit over
-    // so there is no danger of an out of memory error because of this.
-    OBJ_HEADER = IS_64_BIT_JVM ? 16 : 8;
-    ARR_HEADER = IS_64_BIT_JVM ? 24 : 12;
-    OBJ_REF = IS_64_BIT_JVM ? 8 : 4;
-    PADDING = 4;
-    OBJ_OVERHEAD = OBJ_HEADER + OBJ_REF + INT_FIELD + ARR_HEADER + PADDING;
-  }
 
   public static long estimatedSizeOf(byte[] bytes) {
     return ARR_HEADER + bytes.length;
   }
 
   public static long estimatedSizeOf(String s) {
-    return s.length() * 2 + OBJ_OVERHEAD;
+    return s.length() * 2L + OBJ_OVERHEAD;
   }
 
   public static long objOverhead() {
