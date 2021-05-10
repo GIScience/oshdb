e5d9434a41ccbf38074c1eefcdab01cbe5ea3198
Martin Raifer <martin@raifer.tech>
2021-05-10 11:41:21 +0200

    drop unused import

--- oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/time/OSHDBTimestampInterval.java
+++ oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/time/OSHDBTimestampInterval.java
@@ -1,7 +1,6 @@
 package org.heigit.ohsome.oshdb.util.time;
 
 import java.io.Serializable;
-import java.util.Comparator;
 import java.util.Objects;
 import java.util.SortedSet;
 import javax.annotation.Nonnull;
