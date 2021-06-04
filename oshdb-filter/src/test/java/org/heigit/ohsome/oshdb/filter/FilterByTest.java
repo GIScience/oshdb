a5b388801c2d2aea80d650d64f6dcede898e69a7
Martin Raifer <martin@raifer.tech>
2021-06-04 17:41:01 +0200

    add test for (negated) applyOSMGeometry fallback

--- oshdb-filter/src/test/java/org/heigit/ohsome/oshdb/filter/FilterByTest.java
+++ oshdb-filter/src/test/java/org/heigit/ohsome/oshdb/filter/FilterByTest.java
@@ -7,15 +7,26 @@ import java.io.IOException;
 import org.heigit.ohsome.oshdb.osh.OSHNode;
 import org.heigit.ohsome.oshdb.osm.OSMNode;
 import org.junit.Test;
+import org.locationtech.jts.geom.GeometryFactory;
 
 /**
  * Tests helper methods for creating filters from lambda functions.
  */
 public class FilterByTest extends FilterTest {
+  private final GeometryFactory gf = new GeometryFactory();
   private final OSMNode testOSMEntity = createTestOSMEntityNode();
   private final OSHNode testOSHEntity = createTestOSHEntityNode(testOSMEntity);
 
   public FilterByTest() throws IOException {
+  }
+
+  @Test
+  public void testFilterByOSHEntity() {
+    FilterExpression expression;
+    expression = Filter.byOSHEntity(e -> e.getId() == 1);
+    assertTrue(expression.applyOSH(testOSHEntity) && expression.applyOSM(testOSMEntity));
+    expression = expression.negate();
+    assertFalse(expression.applyOSH(testOSHEntity) && expression.applyOSM(testOSMEntity));
   }
 
   @Test
@@ -28,11 +39,9 @@ public class FilterByTest extends FilterTest {
   }
 
   @Test
-  public void testFilterByOSHEntity() {
-    FilterExpression expression;
-    expression = Filter.byOSHEntity(e -> e.getId() == 1);
-    assertTrue(expression.applyOSH(testOSHEntity) && expression.applyOSM(testOSMEntity));
-    expression = expression.negate();
-    assertFalse(expression.applyOSH(testOSHEntity) && expression.applyOSM(testOSMEntity));
+  public void testFilterByOSMEntityApplyGeometryFallback() {
+    FilterExpression expression = Filter.byOSMEntity(e -> e.getVersion() == 1);
+    assertTrue(expression.applyOSMGeometry(testOSMEntity, gf.createPoint()));
+    assertFalse(expression.negate().applyOSMGeometry(testOSMEntity, gf.createPoint()));
   }
 }
