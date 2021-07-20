29e3aee03af7340c0bc1dcae9236b11e61219cfa
Rafael Troilo <rafael.troilo@heigit.org>
2021-07-20 14:20:32 +0200

    add generic OSHDBExcetpion / add ownership and cluster state check to OSHDBIgnite

--- oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/exceptions/OSHDBTagOrRoleNotFoundException.java
+++ oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/exceptions/OSHDBTagOrRoleNotFoundException.java
@@ -3,7 +3,7 @@ package org.heigit.ohsome.oshdb.util.exceptions;
 /**
  * An exception caused by corrupt keytables data.
  */
-public class OSHDBTagOrRoleNotFoundException extends RuntimeException {
+public class OSHDBTagOrRoleNotFoundException extends OSHDBException {
   public OSHDBTagOrRoleNotFoundException(String msg) {
     super(msg);
   }
--- oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/exceptions/OSHDBTimeoutException.java
+++ oshdb-util/src/main/java/org/heigit/ohsome/oshdb/util/exceptions/OSHDBTimeoutException.java
@@ -3,4 +3,5 @@ package org.heigit.ohsome.oshdb.util.exceptions;
 /**
  * An exception caused by a timeout during a query.
  */
-public class OSHDBTimeoutException extends RuntimeException {}
+public class OSHDBTimeoutException extends OSHDBException {
+}
