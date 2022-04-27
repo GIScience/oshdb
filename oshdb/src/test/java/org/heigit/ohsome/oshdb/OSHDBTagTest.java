d0d3f1655f1531511ae4266b0680086f58a02f6a
Rafael Troilo <rafael.troilo@heigit.org>
2022-04-27 16:57:39 +0200

    add missing static import

--- oshdb/src/test/java/org/heigit/ohsome/oshdb/OSHDBTagTest.java
+++ oshdb/src/test/java/org/heigit/ohsome/oshdb/OSHDBTagTest.java
@@ -2,6 +2,7 @@ package org.heigit.ohsome.oshdb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
