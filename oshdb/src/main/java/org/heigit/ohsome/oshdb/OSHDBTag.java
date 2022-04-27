e641f5928c1888afe2ffa02d83f1934e918c266b
Rafael Troilo <rafael.troilo@heigit.org>
2022-04-27 17:07:48 +0200

    add comparable to oshdbtag (#454)
    Co-authored-by: Johannes Visintini <johannes.visintini@heigit.org>

--- oshdb/src/main/java/org/heigit/ohsome/oshdb/OSHDBTag.java
+++ oshdb/src/main/java/org/heigit/ohsome/oshdb/OSHDBTag.java
@@ -1,9 +1,20 @@
 package org.heigit.ohsome.oshdb;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.Objects;
 
-public class OSHDBTag implements Serializable {
+/**
+ * Key/Value id base OSM Tag class.
+ *
+ */
+public class OSHDBTag implements Comparable<OSHDBTag>, Serializable {
+  /**
+   * Order by keyId/valueId, default Comparator for OSHDBTag.
+   */
+  public static final Comparator<OSHDBTag> ORDER_BY_ID = Comparator
+      .comparingInt(OSHDBTag::getKey)
+      .thenComparingInt(OSHDBTag::getValue);
 
   private static final long serialVersionUID = 1L;
   private final int key;
@@ -24,6 +35,11 @@ public class OSHDBTag implements Serializable {
 
   public boolean isPresentInKeytables() {
     return this.value >= 0 && this.key >= 0;
+  }
+
+  @Override
+  public int compareTo(OSHDBTag o) {
+    return ORDER_BY_ID.compare(this, o);
   }
 
   @Override
