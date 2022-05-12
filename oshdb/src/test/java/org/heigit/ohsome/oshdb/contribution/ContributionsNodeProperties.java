222c57daf5a0214762d0a3135f247b305ec65ea3
Johannes Link <business@johanneslink.net>
2022-05-12 14:33:05 +0200

    Added first failing property

---
+++ oshdb/src/test/java/org/heigit/ohsome/oshdb/contribution/ContributionsNodeProperties.java
@@ -0,0 +1,169 @@
+package org.heigit.ohsome.oshdb.contribution;
+
+import java.util.*;
+
+import org.heigit.ohsome.oshdb.*;
+import org.heigit.ohsome.oshdb.osh.*;
+
+import net.jqwik.api.*;
+
+class ContributionsNodeProperties extends OSHDBTest {
+
+	@Property
+	void testOshGeneration(@ForAll List<OSHNode> nodes) {
+
+	}
+
+//  @Test
+//  void testReviveDeletedCreated() {
+//    var versions = nodes(1,
+//        node(3, 3000, 303, 3, tags(tag(1, 1)), 1, 0), //revive
+//        node(-2, 2000, 202, 2, tags(), 0, 0), // deleted
+//        node(1, 1000, 101, 1, tags(tag(1, 1)), 1, 0)); //created
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh);
+//
+//    assertTrue(contribs.hasNext());
+//    var contrib = contribs.next();
+//    assertContrib(3000, 303, 3, versions.get(0), contrib);
+//    assertEquals(EnumSet.of(CREATION), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(2000, 202, 2, versions.get(1), contrib);
+//    assertEquals(EnumSet.of(DELETION), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(1000, 101, 1, versions.get(2), contrib);
+//    assertEquals(EnumSet.of(CREATION), contrib.getTypes());
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testGeomChange() {
+//    var versions = nodes(1,
+//        node(3, 3000, 303, 3, tags(), 1, 0),
+//        node(2, 2000, 202, 2, tags(), 0, 1),
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh);
+//
+//    assertTrue(contribs.hasNext());
+//    var contrib = contribs.next();
+//    assertContrib(3000, 303, 3, versions.get(0), contrib);
+//    assertEquals(EnumSet.of(GEOMETRY_CHANGE), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(2000, 202, 2, versions.get(1), contrib);
+//    assertEquals(EnumSet.of(GEOMETRY_CHANGE), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(1000, 101, 1, versions.get(2), contrib);
+//    assertEquals(EnumSet.of(CREATION), contrib.getTypes());
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testGeomTagCreation() {
+//    var versions = nodes(1,
+//        node(3, 3000, 303, 3, tags(tag(1, 1)), 1, 0), // geom-change
+//        node(2, 2000, 202, 2, tags(tag(1, 1)), 0, 0), // tag-change
+//        node(1, 1000, 101, 1, tags(), 0, 0)); // creation
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh);
+//
+//    assertTrue(contribs.hasNext());
+//    var contrib = contribs.next();
+//    assertContrib(3000, 303, 3, versions.get(0), contrib);
+//    assertEquals(EnumSet.of(GEOMETRY_CHANGE), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(2000, 202, 2, versions.get(1), contrib);
+//    assertEquals(EnumSet.of(TAG_CHANGE), contrib.getTypes());
+//
+//    assertTrue(contribs.hasNext());
+//    contrib = contribs.next();
+//    assertContrib(1000, 101, 1, versions.get(2), contrib);
+//    assertEquals(EnumSet.of(CREATION), contrib.getTypes());
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testSingleVersionAfterMaxTimestamp() {
+//    var versions = nodes(1,
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh, 500);
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testSingleVersionFilterFail() {
+//    var versions = nodes(1,
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh, Long.MAX_VALUE, o -> false);
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testMultipleVersionFilterFail() {
+//    var versions = nodes(1,
+//        node(2, 2000, 202, 2, tags(), 0, 0),
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh, Long.MAX_VALUE, o -> false);
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testNoVersionBeforeMax() {
+//    var versions = nodes(1,
+//        node(2, 2000, 202, 2, tags(), 0, 0),
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh, 500);
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testSquashed() {
+//    var versions = nodes(1,
+//        node(2, 2000, 101, 1, tags(), 0, 0),
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh);
+//
+//    assertTrue(contribs.hasNext());
+//    var contrib = contribs.next();
+//    assertContrib(2000, 101, 1, versions.get(0), contrib);
+//    assertEquals(EnumSet.of(CREATION), contrib.getTypes());
+//
+//    assertFalse(contribs.hasNext());
+//  }
+//
+//  @Test
+//  void testCheckMembersEmpty() {
+//    var versions = nodes(1,
+//        node(1, 1000, 101, 1, tags(), 0, 0));
+//    var osh = osh(versions);
+//    var contribs = Contributions.of(osh);
+//
+//    assertTrue(contribs.hasNext());
+//    var contrib = contribs.next();
+//    assertContrib(1000, 101, 1, versions.get(0), contrib);
+//
+//    assertFalse(contribs.hasNext());
+//  }
+}
