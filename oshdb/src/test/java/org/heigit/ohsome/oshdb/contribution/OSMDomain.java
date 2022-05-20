ffe2f3dbd3c7e2e5b1b975ba42a6d82dc274e18b
Johannes Link <business@johanneslink.net>
2022-05-20 09:09:24 +0200

    Randomized coordinates and tags

--- oshdb/src/test/java/org/heigit/ohsome/oshdb/contribution/OSMDomain.java
+++ oshdb/src/test/java/org/heigit/ohsome/oshdb/contribution/OSMDomain.java
@@ -1,8 +1,5 @@
 package org.heigit.ohsome.oshdb.contribution;
 
-import static org.heigit.ohsome.oshdb.OSHDBTest.tag;
-import static org.heigit.ohsome.oshdb.OSHDBTest.tags;
-
 import java.util.ArrayList;
 import java.util.List;
 import net.jqwik.api.Arbitraries;
@@ -29,24 +26,50 @@ public class OSMDomain extends DomainContextBase {
           Tuple.of(202, 2),
           Tuple.of(303, 3)
       );
-      Arbitrary<int[]> tagArrays = Arbitraries.just(tags(tag(1, 1))); // Randomize
-      Arbitrary<Tuple4<Integer, Integer, Integer, int[]>> versions = Combinators.combine(
-              timestampDiffs, changesets, tagArrays)
-          .as((diff, changeset, tags) -> Tuple.of(diff, changeset.get1(), changeset.get2(), tags));
+      Arbitrary<int[]> tagArrays = tags();
+      Arbitrary<Tuple2<Integer, Integer>> coordinates = coordinates();
+      Arbitrary<Tuple4<Integer, Tuple2<Integer, Integer>, int[], Tuple2<Integer, Integer>>> versions = Combinators.combine(
+          timestampDiffs, changesets, tagArrays, coordinates
+      ).as(Tuple::of);
       return versions.list().ofMinSize(1).ofMaxSize(100)
           .map(tuples -> {
             List<OSMNode> versionList = new ArrayList<>();
             long timestamp = 0;
             int version = 1;
-            for (Tuple4<Integer, Integer, Integer, int[]> tuple : tuples) {
+            for (Tuple4<Integer, Tuple2<Integer, Integer>, int[], Tuple2<Integer, Integer>> tuple : tuples) {
               timestamp += tuple.get1();
-              OSMNode node = OSM.node(id, version, timestamp, tuple.get2(), tuple.get3(),
-                  tuple.get4(), 1, 0);
+              Tuple2<Integer, Integer> changeset = tuple.get2();
+              Tuple2<Integer, Integer> coords = tuple.get4();
+              OSMNode node = OSM.node(
+                  id, version, timestamp, changeset.get1(), changeset.get2(),
+                  tuple.get3(), coords.get1(), coords.get2()
+              );
               versionList.add(node);
               version++;
             }
             return versionList;
           });
     });
+  }
+
+  private Arbitrary<Tuple2<Integer, Integer>> coordinates() {
+    Arbitrary<Integer> longitudes = Arbitraries.integers().between(-10000, 10000); // Real borders?
+    Arbitrary<Integer> latitudes = Arbitraries.integers().between(-10000, 10000); // Real borders?
+    return Combinators.combine(longitudes, latitudes).as(Tuple::of);
+  }
+
+  private Arbitrary<int[]> tags() {
+    Arbitrary<Integer> keys = Arbitraries.integers().between(1, 1000);
+    Arbitrary<Integer> values = Arbitraries.integers().between(1, 1000);
+    return Combinators.combine(keys, values).as(Tuple::of)
+        .list().ofMaxSize(10)
+        .map(listOfTags -> {
+          int[] tags = new int[listOfTags.size() * 2];
+          for (int i = 0; i < listOfTags.size(); i++) {
+            tags[2 * i + 0] = listOfTags.get(i).get1();
+            tags[2 * i + 1] = listOfTags.get(i).get2();
+          }
+          return tags;
+        });
   }
 }
