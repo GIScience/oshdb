bdb936b3fe1951b9cd72ab02174cabf76391fc65
Rafael Troilo <rafael.troilo@heigit.org>
2023-06-19 17:54:00 +0200

    incooperate typefilter for id extraction as well

--- oshdb-filter/src/main/java/org/heigit/ohsome/oshdb/filter/FilterUtil.java
+++ oshdb-filter/src/main/java/org/heigit/ohsome/oshdb/filter/FilterUtil.java
@@ -1,9 +1,10 @@
 package org.heigit.ohsome.oshdb.filter;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.heigit.ohsome.oshdb.osm.OSMType;
 
-import static java.util.Collections.emptyList;
+import java.util.*;
+
+import static java.util.Collections.*;
 import static java.util.Comparator.comparingLong;
 
 public class FilterUtil {
@@ -12,43 +13,53 @@ public class FilterUtil {
         throw new IllegalStateException("utility class");
     }
 
-    public static List<IdRange> ids(FilterExpression expression) {
+    public static Map<EnumSet<OSMType>, List<IdRange>> ids(FilterExpression expression) {
         return ids(expression.normalize());
     }
 
-    public static List<IdRange> ids(List<List<Filter>> normalized) {
-        var ranges = new ArrayList<IdRange>();
-        for (var orGroups : normalized) {
+    public static Map<EnumSet<OSMType>, List<IdRange>> ids(List<List<Filter>> normalized) {
+        var result = new HashMap<EnumSet<OSMType>, List<IdRange>>();
+        for (var group : normalized) {
+            var type = EnumSet.allOf(OSMType.class);
             var groupRanges = new ArrayList<IdRange>();
-            for (var filter: orGroups) {
-                if (filter instanceof IdFilterEquals equals){
-                    groupRanges.add(new IdRange(equals.getId()));
-                } else if (filter instanceof IdFilterEqualsAnyOf equalsAnyOf) {
-                    equalsAnyOf.getIds().stream().map(IdRange::new).forEach(groupRanges::add);
-                } else if (filter instanceof IdFilterRange range) {
-                    groupRanges.add(range.getRange());
+            for (var filter: group) {
+                if (filter instanceof TypeFilter typeFilter) {
+                    type = EnumSet.of(typeFilter.getType());
+                } else if (filter instanceof IdFilterEquals idFilter){
+                    groupRanges.add(new IdRange(idFilter.getId()));
+                } else if (filter instanceof IdFilterEqualsAnyOf idFilter) {
+                    idFilter.getIds().stream().map(IdRange::new).forEach(groupRanges::add);
+                } else if (filter instanceof IdFilterRange idFilter) {
+                    groupRanges.add(idFilter.getRange());
                 }
             }
             if (groupRanges.isEmpty()) {
-                return emptyList();
+                return emptyMap();
             }
-            ranges.addAll(groupRanges);
+            result.computeIfAbsent(type, x -> new ArrayList<>()).addAll(groupRanges);
         }
-        var compact = new ArrayList<IdRange>(ranges.size());
+        compactRanges(result);
+        return result;
+    }
 
-        ranges.sort(comparingLong(IdRange::getFromId));
-        var itr = ranges.iterator();
-        var range = itr.next();
-        while (itr.hasNext()) {
-            var next = itr.next();
-            if (next.getFromId() <= range.getToId() + 1) {
-                range = new IdRange(range.getFromId(), next.getToId());
-            } else {
-                compact.add(range);
-                range = next;
+    private static void compactRanges(HashMap<EnumSet<OSMType>, List<IdRange>> result) {
+        for (var entry : result.entrySet()) {
+            var ranges = entry.getValue();
+            var compact = new ArrayList<IdRange>(ranges.size());
+            ranges.sort(comparingLong(IdRange::getFromId));
+            var itr = ranges.iterator();
+            var range = itr.next();
+            while (itr.hasNext()) {
+                var next = itr.next();
+                if (next.getFromId() <= range.getToId() + 1) {
+                    range = new IdRange(range.getFromId(), next.getToId());
+                } else {
+                    compact.add(range);
+                    range = next;
+                }
             }
+            compact.add(range);
+            entry.setValue(compact);
         }
-        compact.add(range);
-        return compact;
     }
 }
--- oshdb-filter/src/test/java/org/heigit/ohsome/oshdb/filter/FilterUtilTest.java
+++ oshdb-filter/src/test/java/org/heigit/ohsome/oshdb/filter/FilterUtilTest.java
@@ -1,8 +1,11 @@
 package org.heigit.ohsome.oshdb.filter;
 
+import org.heigit.ohsome.oshdb.osm.OSMType;
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,21 +14,44 @@ class FilterUtilTest extends FilterTest{
 
     @Test
     void extractIds() {
-        extractIds("id:123", List.of(new IdRange(123L)));
-        extractIds("id:123 or id:234", List.of(new IdRange(123L), new IdRange(234L)));
-        extractIds("id:123 or id:124", List.of(new IdRange(123L, 124L)));
-        extractIds("id:123 and not id:234", List.of(new IdRange(123L)));
-        extractIds("id:123 or name=heigit", List.of());
-        extractIds("id:(123, 234)", List.of(new IdRange(123L), new IdRange(234L)));
-        extractIds("id:(123 .. 125)", List.of(new IdRange(123L, 125L)));
+        extractIds("id:123", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L))));
+        extractIds("id:123 or id:234", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L), new IdRange(234L))));
+        extractIds("id:123 or id:124", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L, 124L))));
+        extractIds("id:123 and not id:234", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L))));
+        extractIds("id:123 or name=heigit", Map.of());
+        extractIds("id:(123, 234)", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L), new IdRange(234L))));
+        extractIds("id:(123 .. 125)", Map.of(EnumSet.allOf(OSMType.class),List.of(new IdRange(123L, 125L))));
     }
 
-    private void extractIds(String filter, List<IdRange> expected) {
+    @Test
+    void extractIdsWithTypes() {
+        extractIds("id:node/123 or id:way/234", Map.of(
+                EnumSet.of(OSMType.NODE), List.of(new IdRange(123L)),
+                EnumSet.of(OSMType.WAY), List.of(new IdRange(234L))));
+        extractIds("id:node/123 or id:way/234 or id:way/235", Map.of(
+                EnumSet.of(OSMType.NODE), List.of(new IdRange(123L)),
+                EnumSet.of(OSMType.WAY), List.of(new IdRange(234L, 235L))));
+        extractIds("id:node/123 or (type:way and id:234)", Map.of(
+                EnumSet.of(OSMType.NODE), List.of(new IdRange(123L)),
+                EnumSet.of(OSMType.WAY), List.of(new IdRange(234L))));
+        extractIds("id:node/123 or id:234", Map.of(
+                EnumSet.of(OSMType.NODE), List.of(new IdRange(123L)),
+                EnumSet.allOf(OSMType.class), List.of(new IdRange(234L))));
+        extractIds("not type:node and id:123", Map.of(
+                EnumSet.of(OSMType.WAY), List.of(new IdRange(123L)),
+                EnumSet.of(OSMType.RELATION), List.of(new IdRange(123L))));
+    }
+
+    private void extractIds(String filter, Map<EnumSet<OSMType>,List<IdRange>> expected) {
        var expression = parser.parse(filter);
        var actual = FilterUtil.ids(expression);
 
        assertEquals(expected.size(), actual.size());
-       assertTrue(expected.containsAll(actual));
+       for (var entry : expected.entrySet()) {
+           assertTrue(actual.containsKey(entry.getKey()));
+           assertEquals(entry.getValue().size(), actual.get(entry.getKey()).size());
+           assertTrue(actual.get(entry.getKey()).containsAll(entry.getValue()));
+       }
     }
 
 }
